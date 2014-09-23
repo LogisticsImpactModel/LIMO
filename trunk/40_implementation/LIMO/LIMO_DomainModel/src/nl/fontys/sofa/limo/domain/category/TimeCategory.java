54342950769cbf289783e235041a5858b436b2c7
2182242 <2182242@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-09-23 13:34:37 +0000

    - Parts of domain model

---
+++ trunk/40_implementation/LIMO/LIMO_DomainModel/src/nl/fontys/sofa/limo/domain/category/TimeCategory.java
@@ -0,0 +1,13 @@
+package nl.fontys.sofa.limo.domain.category;
+
+/**
+ *
+ * @author Dominik Kaisers <d.kaisers@student.fontys.nl>
+ */
+public class TimeCategory extends Category{
+
+    public TimeCategory(String identifier) {
+        super(identifier);
+    }
+    
+}
