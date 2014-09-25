82b1d568584056e1bcd3303b03b6eb2b6c8dba25
2182242 <2182242@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-09-25 10:29:50 +0000

    - Changed some stuff in component for reasons - Added Event stuff

---
+++ trunk/40_implementation/LIMO/LIMO_DomainModel/src/nl/fontys/sofa/limo/domain/events/EventExecutionStateDependency.java
@@ -0,0 +1,13 @@
+package nl.fontys.sofa.limo.domain.events;
+
+/**
+ *
+ * @author Dominik Kaisers <d.kaisers@student.fontys.nl>
+ */
+public enum EventExecutionStateDependency {
+    
+    INDEPENDENT,
+    EXECUTED,
+    NOT_EXECUTED;
+    
+}
