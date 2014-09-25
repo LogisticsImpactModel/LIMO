d958cd0c711762e16a92773d5df5ee913fb641a1
2184008 <2184008@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-09-25 13:59:53 +0000

    more tests and constructors in categories

--- LIMO/LIMO_DomainModel/src/main/java/nl/fontys/sofa/limo/domain/category/CostCategory.java
+++ LIMO/LIMO_DomainModel/src/main/java/nl/fontys/sofa/limo/domain/category/CostCategory.java
@@ -5,5 +5,14 @@ package nl.fontys.sofa.limo.domain.category;
  * @author Dominik Kaisers <d.kaisers@student.fontys.nl>
  */
 public class CostCategory extends Category {
-    
+
+    public CostCategory() {
+        super();
+    }
+
+    public CostCategory(String identifier) {
+        super();
+        this.identifier = identifier;
+    }
+
 }
--- LIMO/LIMO_DomainModel/src/main/java/nl/fontys/sofa/limo/domain/category/TimeCategory.java
+++ LIMO/LIMO_DomainModel/src/main/java/nl/fontys/sofa/limo/domain/category/TimeCategory.java
@@ -4,6 +4,15 @@ package nl.fontys.sofa.limo.domain.category;
  *
  * @author Dominik Kaisers <d.kaisers@student.fontys.nl>
  */
-public class TimeCategory extends Category{
-    
+public class TimeCategory extends Category {
+
+    public TimeCategory() {
+        super();
+    }
+
+    public TimeCategory(String identifier) {
+        super();
+        this.identifier = identifier;
+    }
+
 }
