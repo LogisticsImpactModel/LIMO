59f50ec333d385f25792b22e49011e99999bbc04
2182242 <2182242@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-09-25 09:46:51 +0000

    - Adding domain model stuff (!)

---
+++ trunk/40_implementation/LIMO/LIMO_DomainModel/src/nl/fontys/sofa/limo/domain/Component.java
@@ -0,0 +1,85 @@
+package nl.fontys.sofa.limo.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import nl.fontys.sofa.limo.domain.events.Event;
+
+/**
+ *
+ * @author Dominik Kaisers <d.kaisers@student.fontys.nl>
+ */
+public abstract class Component {
+    
+    protected String identifier;
+    protected List<Entry> costs;
+    protected List<Entry> leadTimes;
+    protected List<Entry> delays;
+    protected List<Event> events;
+    
+    /**
+     * Actual bytes the icon is composed of.
+     */
+    protected byte[] icon;
+
+    public Component(String identifier) {
+        this(identifier, new ArrayList<Entry>(), new ArrayList<Entry>(), new ArrayList<Entry>(), new ArrayList<Event>(), new byte[]{});
+    }
+
+    public Component(String identifier, List<Entry> costs, List<Entry> leadTimes, List<Entry> delays, List<Event> events, byte[] icon) {
+        this.identifier = identifier;
+        this.costs = costs;
+        this.leadTimes = leadTimes;
+        this.delays = delays;
+        this.events = events;
+        this.icon = icon;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public List<Entry> getCosts() {
+        return costs;
+    }
+
+    public void setCosts(List<Entry> costs) {
+        this.costs = costs;
+    }
+
+    public List<Entry> getLeadTimes() {
+        return leadTimes;
+    }
+
+    public void setLeadTimes(List<Entry> leadTimes) {
+        this.leadTimes = leadTimes;
+    }
+
+    public List<Entry> getDelays() {
+        return delays;
+    }
+
+    public void setDelays(List<Entry> delays) {
+        this.delays = delays;
+    }
+    
+    public byte[] getIcon() {
+        return icon;
+    }
+
+    public void setIcon(byte[] icon) {
+        this.icon = icon;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+    
+}
---
+++ trunk/40_implementation/LIMO/LIMO_DomainModel/src/nl/fontys/sofa/limo/domain/events/Event.java
@@ -0,0 +1,15 @@
+package nl.fontys.sofa.limo.domain.events;
+
+import nl.fontys.sofa.limo.domain.Component;
+
+/**
+ *
+ * @author Dominik Kaisers <d.kaisers@student.fontys.nl>
+ */
+public class Event extends Component {
+
+    public Event(String identifier) {
+        super(identifier);
+    }
+    
+}
