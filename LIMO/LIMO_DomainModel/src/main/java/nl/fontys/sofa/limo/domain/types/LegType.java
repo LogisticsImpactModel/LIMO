9d9e9c02c34b510bce74307e864d958e96e6d46b
2192656 <2192656@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-09-26 12:35:45 +0000

    - Icon is now BasEntity - Type now holds an Icon

--- LIMO/LIMO_DomainModel/src/main/java/nl/fontys/sofa/limo/domain/types/LegType.java
+++ LIMO/LIMO_DomainModel/src/main/java/nl/fontys/sofa/limo/domain/types/LegType.java
@@ -2,6 +2,7 @@ package nl.fontys.sofa.limo.domain.types;
 
 import java.util.List;
 import nl.fontys.sofa.limo.domain.Entry;
+import nl.fontys.sofa.limo.domain.Icon;
 
 /**
  *
@@ -9,8 +10,11 @@ import nl.fontys.sofa.limo.domain.Entry;
  */
 public class LegType extends Type {
 
-    public LegType(String identifier, List<Entry> costs, List<Entry> leadTimes, List<Entry> delays, String iconID) {
-        super(identifier, costs, leadTimes, delays, iconID);
+    public LegType(String identifier, List<Entry> costs, List<Entry> leadTimes, List<Entry> delays) {
+        super(identifier, costs, leadTimes, delays);
     }
 
+    public LegType(String identifier, List<Entry> costs, List<Entry> leadTimes, List<Entry> delays, Icon icon) {
+        super(identifier, costs, leadTimes, delays, icon);
+    }
 }
