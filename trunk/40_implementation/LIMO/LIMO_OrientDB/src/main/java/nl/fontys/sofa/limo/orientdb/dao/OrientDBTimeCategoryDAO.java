ba246b589e29aab3783a24ce58c51e749eb413f5
2178880 <2178880@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-09-26 10:02:13 +0000

    TimeCategory DAO + Test

--- trunk/40_implementation/LIMO/LIMO_OrientDB/src/main/java/nl/fontys/sofa/limo/orientdb/dao/OrientDBTimeCategoryDAO.java
+++ trunk/40_implementation/LIMO/LIMO_OrientDB/src/main/java/nl/fontys/sofa/limo/orientdb/dao/OrientDBTimeCategoryDAO.java
@@ -5,47 +5,90 @@
  */
 package nl.fontys.sofa.limo.orientdb.dao;
 
+import com.orientechnologies.orient.core.id.ORecordId;
+import com.orientechnologies.orient.core.iterator.ORecordIteratorClass;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import nl.fontys.sofa.limo.api.dao.TimeCategoryDAO;
 import nl.fontys.sofa.limo.domain.category.TimeCategory;
+import nl.fontys.sofa.limo.orientdb.OrientDBMapper;
 import nl.fontys.sofa.limo.orientdb.database.OrientDBAccess;
 
-public class OrientDBTimeCategoryDAO implements TimeCategoryDAO {
+public class OrientDBTimeCategoryDAO extends OrientDBAbstractDAO implements TimeCategoryDAO, OrientDBMapper<TimeCategory> {
 
-    private final OrientDBAccess orientDBAccess;
+    private static final String TABLE_NAME = "TimeCategories";
 
     public OrientDBTimeCategoryDAO(OrientDBAccess orientDBAccess) {
-        this.orientDBAccess = orientDBAccess;
+        super(orientDBAccess, TABLE_NAME);
     }
 
     @Override
     public List<TimeCategory> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ORecordIteratorClass<ODocument> results = orientDBAccess.getConnection().browseClass(getTableName());
+        ArrayList<TimeCategory> resultList = new ArrayList<>();
+
+        for (ODocument doc : results) {
+            resultList.add(map(doc));
+        }
+
+        return resultList;
     }
 
     @Override
     public TimeCategory findById(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        OSQLSynchQuery<ODocument> query = new OSQLSynchQuery<>("select * from " + id);
+        List<ODocument> results = orientDBAccess.getConnection().query(query);
+        return map(results.get(0));
     }
 
     @Override
     public void insert(TimeCategory entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        entity.setLastUpdate(new Date().getTime());
+        ODocument doc = map(entity).save();
+        entity.setId(doc.getIdentity().toString());
     }
 
     @Override
     public boolean update(TimeCategory entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        entity.setLastUpdate(new Date().getTime());
+        map(entity).save();
+        return true;
     }
 
     @Override
     public boolean delete(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        orientDBAccess.getConnection().delete(new ORecordId(id));
+        return true;
     }
 
     @Override
     public String getTableName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return TABLE_NAME;
+    }
+
+    @Override
+    public TimeCategory map(ODocument doc) {
+        TimeCategory tc = new TimeCategory();
+        tc.setId(doc.getIdentity().toString());
+        tc.setLastUpdate((long) doc.field("lastUpdate"));
+        tc.setIdentifier((String) doc.field("identifier"));
+        return tc;
+    }
+
+    @Override
+    public ODocument map(TimeCategory entity) {
+        ODocument doc;
+        if (entity.getId() != null) {
+            doc = new ODocument(getTableName(), new ORecordId(entity.getId()));
+        } else {
+            doc = new ODocument(getTableName());
+        }
+        doc.field("lastUpdate", entity.getLastUpdate());
+        doc.field("identifier", entity.getIdentifier());
+        return doc;
     }
 
 }
