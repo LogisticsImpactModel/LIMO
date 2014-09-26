73abc7cfaab5a0ea705d9b95ea582e2cb6849114
2182242 <2182242@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-09-26 09:45:06 +0000

    - Imports cleaned up

--- LIMO/LIMO_OrientDB/src/main/java/nl/fontys/sofa/limo/orientdb/dao/OrientDBAbstractDAO.java
+++ LIMO/LIMO_OrientDB/src/main/java/nl/fontys/sofa/limo/orientdb/dao/OrientDBAbstractDAO.java
@@ -1,11 +1,6 @@
 package nl.fontys.sofa.limo.orientdb.dao;
 
-import com.orientechnologies.orient.core.iterator.ORecordIteratorClass;
 import com.orientechnologies.orient.core.metadata.schema.OSchema;
-import com.orientechnologies.orient.core.record.impl.ODocument;
-import java.util.ArrayList;
-import nl.fontys.sofa.limo.domain.BaseEntity;
-import nl.fontys.sofa.limo.domain.category.CostCategory;
 import nl.fontys.sofa.limo.orientdb.database.OrientDBAccess;
 
 /**
