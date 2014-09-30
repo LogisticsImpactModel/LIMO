3832651e1de9b36e43d1beb1f7a6456709b71fbf
2194479 <2194479@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-09-30 07:53:46 +0000

    finalized iconTest, coverage not yet checked

--- trunk/40_implementation/LIMO/LIMO_DomainModel/src/test/java/nl/fontys/sofa/limo/domain/IconTest.java
+++ trunk/40_implementation/LIMO/LIMO_DomainModel/src/test/java/nl/fontys/sofa/limo/domain/IconTest.java
@@ -59,7 +59,10 @@ public class IconTest {
         assertNull("There should be no Image object because of an empty byte array",ic1.getIcon());
         //testing w/ method w/ URLstring incorrect
         Icon ic2 = new Icon("C:\notExisting.crap");
-        assertNull("There should be nog Image object because of an incorrect URL",ic2.getIcon());
+        assertNull("There should be no Image object because of an incorrect URL",ic2.getIcon());
+        //testing w/ method w/ URLstring correct
+        Icon icX = new Icon("C:/icon.jpg");
+        assertNotNull("There should be an Image object because of a correct URL",icX.getIcon());
         //lead to existing file to convert to bytes
         File fi = new File("C:/icon.jpg");
         try {
@@ -71,11 +74,9 @@ public class IconTest {
             assertEquals("Identical img heights",origImg.getHeight(),img1.getHeight(),1);
             System.out.println("Height img: "+img1.getHeight());
         } catch (IOException ex) {
-            System.out.println("Could not locate image");
-        }
-        
+            fail("Could not locate image");
+        }   
     }
-
     /**
      * Test of getIconBytes method, of class Icon.
      */
@@ -90,47 +91,63 @@ public class IconTest {
             System.out.println("Image loaded");
             Assert.assertArrayEquals("Identical byteArrays",fileContent,ic3.getIconBytes());
         } catch (IOException ex) {
-            System.out.println("Could not locate image");
+            fail("Could not locate image");
         }
     }
-
     /**
      * Test of setIcon method, of class Icon.
      */
     @Test
     public void testSetIcon_byteArr() {
         System.out.println("setIcon");
-        byte[] icon = null;
-        Icon instance = new Icon();
-        instance.setIcon(icon);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+                File fi = new File("C:/icon.jpg");
+        try {
+            fileContent = Files.readAllBytes(fi.toPath());
+            System.out.println("Image loaded");
+            Icon ic3 = new Icon();//create icon w/ no params, so empty
+            ic3.setIcon(fileContent);
+            Assert.assertArrayEquals("Identical byteArrays",fileContent,ic3.getIconBytes());
+        } catch (IOException ex) {
+            fail("Could not locate image");
+        }
     }
-
     /**
      * Test of setIcon method, of class Icon.
      */
     @Test
     public void testSetIcon_Image() {
         System.out.println("setIcon");
-        Image image = null;
-        Icon instance = new Icon();
-        //instance.setIcon(image);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        File fi = new File("C:/icon.jpg");
+        try {
+            fileContent = Files.readAllBytes(fi.toPath());
+            System.out.println("Image loaded");
+            Icon ic3 = new Icon();//new empty icon
+            BufferedImage origImg = ImageIO.read(new ByteArrayInputStream(fileContent));
+            ic3.setIcon(origImg);//set image into icon using setIcon
+            BufferedImage img1 = (BufferedImage) ic3.getIcon();
+            
+            assertEquals("Identical img heights",origImg.getHeight(),img1.getHeight(),1);
+            System.out.println("Height img: "+img1.getHeight());
+        } catch (IOException ex) {
+            fail("Img could not be loaded at testSetIcon_Image");
+        }
     }
-
     /**
      * Test of setImage method, of class Icon.
      */
     @Test
     public void testSetImage() {
         System.out.println("setImage");
-        String iconPath = "";
-        Icon instance = new Icon();
-        instance.setImage(iconPath);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        Icon ic1 = new Icon();//create empty icon
+        ic1.setImage("C:/icon.jpg");//then use setImage to fill the icon object
+        BufferedImage img1 = (BufferedImage) ic1.getIcon();//get the image from icon object back
+        File fi = new File("C:/icon.jpg");//make file to compare images
+        try {
+            fileContent = Files.readAllBytes(fi.toPath());
+            BufferedImage origImg = ImageIO.read(new ByteArrayInputStream(fileContent));
+            assertEquals("Heights should be equal",img1.getHeight(),origImg.getHeight(),1);//comparing heights of artificially made image (origImg) and img using setter
+        } catch(IOException ex) {
+            fail("Could not locate test icon image at URL");
+        }
     }
-    
 }
