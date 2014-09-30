1000038a927ec2cec64c3a1e3f602bea5ffbf2dc
2192656 <2192656@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-09-30 08:47:58 +0000

    - Added Location class -- Continents Enum -Hub got a Location

---
+++ trunk/40_implementation/LIMO/LIMO_DomainModel/src/main/java/nl/fontys/sofa/limo/domain/location/Continents.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.fontys.sofa.limo.domain.location;
+
+/**
+ *
+ * @author Matthias Brück
+ */
+public enum Continents {
+    AFRICA,
+    ASIA,
+    AUSTRALIA,
+    EUROPE,
+    NORTH_AMERICA,
+    SOUTH_AMERICA,
+    ANTARCITCA;
+}
