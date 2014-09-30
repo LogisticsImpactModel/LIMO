6a7037411e76406cf889527dec04ba89013c58a4
2178880 <2178880@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-09-30 10:33:32 +0000

    LegDAO

--- LIMO/LIMO_OrientDB/src/main/java/nl/fontys/sofa/limo/orientdb/dao/OrientDBLegDAO.java
+++ LIMO/LIMO_OrientDB/src/main/java/nl/fontys/sofa/limo/orientdb/dao/OrientDBLegDAO.java
@@ -5,27 +5,13 @@
  */
 package nl.fontys.sofa.limo.orientdb.dao;
 
-import com.orientechnologies.orient.core.record.impl.ODocument;
-import java.util.List;
-import nl.fontys.sofa.limo.api.dao.EventDAO;
 import nl.fontys.sofa.limo.api.dao.LegDAO;
-import nl.fontys.sofa.limo.domain.component.Event;
 import nl.fontys.sofa.limo.domain.component.Leg;
 import nl.fontys.sofa.limo.orientdb.database.OrientDBAccess;
 
 public class OrientDBLegDAO extends OrientDBAbstractDAO<Leg> implements LegDAO {
 
     public OrientDBLegDAO(OrientDBAccess orientDBAccess) {
-        super(orientDBAccess, "Legs");
+        super(orientDBAccess, Leg.class);
     }
-    @Override
-    public Leg map(ODocument doc) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public ODocument map(Leg entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
 }
