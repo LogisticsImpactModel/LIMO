a518f114fcbd74c8f15fd591a30a00cdf0fb60e2
2042635 <2042635@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-10-03 21:58:19 +0000

    rename incorrect named packages

---
+++ LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/node/CostCategoryNode.java
@@ -0,0 +1,21 @@
+package nl.fontys.sofa.limo.view.node;
+
+import java.beans.IntrospectionException;
+import nl.fontys.sofa.limo.domain.category.CostCategory;
+import org.openide.nodes.BeanNode;
+
+/**
+ * View representation of the CostCategory class. 
+ *
+ * @author Sebastiaan Heijmann
+ */
+public class CostCategoryNode extends BeanNode{
+
+	public CostCategoryNode(CostCategory bean) throws IntrospectionException {
+		super(bean);
+		setDisplayName(bean.getIdentifier());
+		setShortDescription(bean.getDescription());
+	}
+	
+
+}
---
+++ LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/node/CostCategoryRootNode.java
@@ -0,0 +1,63 @@
+package nl.fontys.sofa.limo.view.node;
+
+import java.io.IOException;
+import javax.swing.Action;
+import nl.fontys.sofa.limo.api.dao.CostCategoryDAO;
+import nl.fontys.sofa.limo.api.dao.DAOFactory;
+import nl.fontys.sofa.limo.domain.category.CostCategory;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.actions.NewAction;
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.Children;
+import org.openide.util.Lookup;
+import org.openide.util.actions.SystemAction;
+import org.openide.util.datatransfer.NewType;
+
+/**
+ * Root node class for cost category. Defines actions which can be invoked.
+ *
+ * @author Sebastiaan Heijmann
+ */
+public class CostCategoryRootNode extends AbstractNode{
+
+	public CostCategoryRootNode(Children children) {
+		super(children);
+	}
+
+	@Override
+	public Action[] getActions(boolean context) {
+		return new Action[]{SystemAction.get(NewAction.class)};
+	}
+
+	@Override
+	public NewType[] getNewTypes() {
+		return new NewType[]{new NewType(){
+
+			@Override
+			public String getName() {
+				return "Cost Category";
+			}
+
+			@Override
+			public void create() throws IOException {
+				NotifyDescriptor.InputLine name =
+								new NotifyDescriptor.InputLine("Name",
+												"New Cost Category");
+				NotifyDescriptor.InputLine description =
+								new NotifyDescriptor.InputLine("Description",
+												"New Cost Category");
+				DialogDisplayer.getDefault().notify(name);
+				DialogDisplayer.getDefault().notify(description);
+
+				CostCategory cc = new CostCategory();
+				cc.setIdentifier(name.getInputText());
+				cc.setDescription(description.getInputText());
+				
+				DAOFactory df = Lookup.getDefault().lookup(DAOFactory.class);
+				CostCategoryDAO ccd = df.getCostCategoryDAO();
+				ccd.insert(cc);
+			}
+		}};
+	}
+}
