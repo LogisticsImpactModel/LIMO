5febabcc1be12fc96f8a1c81efada19439bb1d5d
2184008 <2184008@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-10-03 13:49:13 +0000

    update tabbed component entries component

--- trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/custom/NewClass.java
+++ trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/custom/NewClass.java
@@ -1,11 +1,12 @@
 package nl.fontys.sofa.limo.view.custom;
 
 import java.awt.BorderLayout;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JFrame;
-import nl.fontys.sofa.limo.api.dao.DAOFactory;
-import org.openide.util.Lookup;
-import org.openide.util.lookup.Lookups;
+import nl.fontys.sofa.limo.domain.Entry;
+import nl.fontys.sofa.limo.domain.component.Event;
+import nl.fontys.sofa.limo.domain.value.SingleValue;
 
 public class NewClass {
 
@@ -13,19 +14,36 @@ public class NewClass {
         JFrame frame = new JFrame("TabbedPaneDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
-
-        //Add content to the window.
         frame.setLayout(new BorderLayout());
-        frame.add(new TabbedComponentEntries(), BorderLayout.CENTER);
+
+        frame.add(new TabbedComponentEntries(getEvent()), BorderLayout.CENTER);
 
         //Display the window.
         frame.pack();
         frame.setVisible(true);
+    }
 
-        Collection<? extends DAOFactory> lookupAll = Lookup.getDefault().lookupAll(DAOFactory.class);
-        DAOFactory factory = (DAOFactory) lookupAll.toArray()[0];
-        factory.getIconDAO();
-        DAOFactory lookup = Lookup.getDefault().lookup(DAOFactory.class);
-        lookup.getIconDAO();
+    private static Event getEvent() {
+        Event event = new Event("Pirates");
+
+        ArrayList<Entry> costs = new ArrayList<>();
+        Entry costEntry = new Entry("Shipping", "Transport");
+        costEntry.setValue(new SingleValue(20000));
+        costs.add(costEntry);
+        event.setCosts(costs);
+
+        ArrayList<Entry> delays = new ArrayList<>();
+        Entry delayEntry = new Entry("Pirate Attack", "Unforeseeable");
+        delayEntry.setValue(new SingleValue(250000));
+        delays.add(delayEntry);
+        event.setDelays(delays);
+
+        List<Entry> leadTimes = new ArrayList<>();
+        Entry leadTimeEntry = new Entry("", "");
+        leadTimeEntry.setValue(new SingleValue(3000));
+        leadTimes.add(leadTimeEntry);
+        event.setLeadTimes(leadTimes);
+
+        return event;
     }
 }
