9d3b9961f0cfff2ef2faed37fbe37474e69e1701
2042635 <2042635@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-10-03 10:21:33 +0000

    removed unnecessary dependendies

--- LIMO/LIMO_OrientDB/src/main/java/nl/fontys/sofa/limo/orientdb/dao/OrientDBHubTypeDAO.java
+++ LIMO/LIMO_OrientDB/src/main/java/nl/fontys/sofa/limo/orientdb/dao/OrientDBHubTypeDAO.java
@@ -1,10 +1,8 @@
 package nl.fontys.sofa.limo.orientdb.dao;
 
-import java.util.List;
 import nl.fontys.sofa.limo.api.dao.HubTypeDAO;
 import nl.fontys.sofa.limo.domain.types.HubType;
 import nl.fontys.sofa.limo.orientdb.database.OrientDBAccess;
-import org.openide.util.lookup.ServiceProvider;
 
 public class OrientDBHubTypeDAO extends OrientDBAbstractDAO<HubType> implements HubTypeDAO {
 
--- LIMO/LIMO_view/src/main/java/nl/fontys/limo/view/factory/CostCategoryChildFactory.java
+++ LIMO/LIMO_view/src/main/java/nl/fontys/limo/view/factory/CostCategoryChildFactory.java
@@ -8,10 +8,14 @@ package nl.fontys.limo.view.factory;
 
 import java.beans.IntrospectionException;
 import java.util.List;
+import nl.fontys.sofa.limo.api.dao.CostCategoryDAO;
+import nl.fontys.sofa.limo.api.dao.DAOFactory;
+import nl.fontys.sofa.limo.domain.category.CostCategory;
 import org.openide.nodes.BeanNode;
 import org.openide.nodes.ChildFactory;
 import org.openide.nodes.Node;
 import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
 
 /**
  *
@@ -21,15 +25,15 @@ public class CostCategoryChildFactory extends ChildFactory<String>{
 
 	@Override
 	protected boolean createKeys(List<String> list) {
-//		DAOFactory df = Lookup.getDefault().lookup(DAOFactory.class);
-//		CostCategoryDAO ccd = df.getCostCategoryDAO();
-//		List<CostCategory> ccl = ccd.findAll();
-//		for(CostCategory cc : ccl){
-//			list.add(cc.getIdentifier());
-//		}
-		for(int i = 0; i < 5; i++){
-			list.add("Test");
+		DAOFactory df = Lookup.getDefault().lookup(DAOFactory.class);
+		CostCategoryDAO ccd = df.getCostCategoryDAO();
+		List<CostCategory> ccl = ccd.findAll();
+		for(CostCategory cc : ccl){
+			list.add(cc.getIdentifier());
 		}
+//		for(int i = 0; i < 5; i++){
+//			list.add("Test");
+//		}
 		return true;
 	}
 
