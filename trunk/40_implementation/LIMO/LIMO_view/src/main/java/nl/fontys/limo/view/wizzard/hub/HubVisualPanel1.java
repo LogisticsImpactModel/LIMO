ad689b506626dd6d89a3e4af51b8a440818cf102
2178880 <2178880@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-10-03 12:42:09 +0000

    Hub Wizzard

---
+++ trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/limo/view/wizzard/hub/HubVisualPanel1.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.fontys.limo.view.wizzard.hub;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.GroupLayout;
+import javax.swing.JPanel;
+import nl.fontys.sofa.limo.api.dao.HubTypeDAO;
+import nl.fontys.sofa.limo.domain.types.HubType;
+
+public final class HubVisualPanel1 extends JPanel {
+
+    /**
+     * Creates new form HubVisualPanel1
+     */
+    public HubVisualPanel1() {
+        initComponents();
+    }
+
+    @Override
+    public String getName() {
+        return "Add Hub";
+    }
+
+    private void initComponents() {
+
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        rbFromScratch = new javax.swing.JRadioButton();
+        rbFromHubType = new javax.swing.JRadioButton();
+        cmbHubType = new javax.swing.JComboBox();
+
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        buttonGroup1.add(rbFromScratch);
+        rbFromScratch.setText("From Scratch");
+        add(rbFromScratch, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 50, -1, -1));
+        rbFromScratch.setSelected(true);
+        rbFromScratch.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (rbFromScratch.isSelected()) {
+                    cmbHubType.setEnabled(false);
+                }
+            }
+        });
+
+        buttonGroup1.add(rbFromHubType);
+        rbFromHubType.setText("From Hub Type");
+        add(rbFromHubType, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 80, -1, -1));
+        rbFromHubType.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (rbFromHubType.isSelected()) {
+                    cmbHubType.setEnabled(true);
+                }
+            }
+        });
+
+        cmbHubType.setModel(new javax.swing.DefaultComboBoxModel(new String[]{"Item 1", "Item 2", "Item 3", "Item 4"}));
+        add(cmbHubType, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 110, 240, -1));
+        cmbHubType.setEnabled(false);
+    }
+
+    public HubType getHubType() {
+        if (rbFromHubType.isSelected()) {
+            return hubTypes.get(cmbHubType.getSelectedIndex());
+        } else {
+            return new HubType();
+        }
+    }
+
+    javax.swing.ButtonGroup buttonGroup1;
+    javax.swing.JComboBox cmbHubType;
+    javax.swing.JRadioButton rbFromHubType;
+    javax.swing.JRadioButton rbFromScratch;
+    private ArrayList<HubType> hubTypes;
+}
---
+++ trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/limo/view/wizzard/hub/HubVisualPanel2.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.fontys.limo.view.wizzard.hub;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import nl.fontys.sofa.limo.domain.types.HubType;
+import org.openide.WizardDescriptor;
+
+public final class HubVisualPanel2 extends JPanel {
+
+    /**
+     * Creates new form HubVisualPanel2
+     */
+    public HubVisualPanel2() {
+        initComponents();
+    }
+
+    @Override
+    public String getName() {
+        return "Set Name and Icon";
+    }
+
+    private void initComponents() {
+
+        lblName = new javax.swing.JLabel();
+        tfName = new javax.swing.JTextField();
+        lblIcon = new javax.swing.JLabel();
+        lblPreview = new javax.swing.JLabel();
+        btnSelect = new javax.swing.JButton();
+        fc = new JFileChooser();
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        org.openide.awt.Mnemonics.setLocalizedText(lblName, org.openide.util.NbBundle.getMessage(HubVisualPanel2.class, "HubVisualPanel2.lblName.text_1")); // NOI18N
+        add(lblName, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, -1, 30));
+
+        tfName.setText(org.openide.util.NbBundle.getMessage(HubVisualPanel2.class, "HubVisualPanel2.tfName.text_1")); // NOI18N
+        add(tfName, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 30, 200, -1));
+
+        org.openide.awt.Mnemonics.setLocalizedText(lblIcon, org.openide.util.NbBundle.getMessage(HubVisualPanel2.class, "HubVisualPanel2.lblIcon.text_1")); // NOI18N
+        add(lblIcon, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 110, -1, 30));
+
+        org.openide.awt.Mnemonics.setLocalizedText(lblPreview, org.openide.util.NbBundle.getMessage(HubVisualPanel2.class, "HubVisualPanel2.lblPreview.text_1")); // NOI18N
+        add(lblPreview, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 100, 60, 60));
+
+        org.openide.awt.Mnemonics.setLocalizedText(btnSelect, org.openide.util.NbBundle.getMessage(HubVisualPanel2.class, "HubVisualPanel2.btnSelect.text_1")); // NOI18N
+        btnSelect.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int returnVal = fc.showOpenDialog(HubVisualPanel2.this);
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    icon = fc.getSelectedFile();
+                }
+            }
+        });
+        add(btnSelect, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 110, 130, -1));
+
+    }
+
+    public void updatePanel() {
+        if (hubType == null) {
+            JOptionPane.showMessageDialog(this, "Leer");
+        }
+    }
+
+    public void setHubType(HubType hubType) {
+        this.hubType = hubType;
+    }
+
+    public File getIcon() {
+        return icon;
+    }
+
+    private javax.swing.JButton btnSelect;
+    private javax.swing.JLabel lblIcon;
+    private javax.swing.JLabel lblName;
+    private javax.swing.JLabel lblPreview;
+    private javax.swing.JTextField tfName;
+    private JFileChooser fc;
+    private HubType hubType;
+    private File icon;
+}
