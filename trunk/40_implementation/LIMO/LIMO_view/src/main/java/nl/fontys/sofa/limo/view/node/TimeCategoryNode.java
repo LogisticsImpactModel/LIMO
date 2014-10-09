ceb8ec8c665140e32220448294b696b740f51975
2182242 <2182242@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-10-09 09:24:03 +0000

    - Icon caching for nodes

--- trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/node/TimeCategoryNode.java
+++ trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/node/TimeCategoryNode.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.Action;
 import nl.fontys.sofa.limo.domain.category.TimeCategory;
+import nl.fontys.sofa.limo.view.util.IconUtil;
 import org.openide.actions.DeleteAction;
 import org.openide.nodes.BeanNode;
 import org.openide.util.Exceptions;
@@ -13,25 +14,25 @@ import org.openide.util.actions.SystemAction;
 
 /**
  * View representation of the TimeCategory class.
- * 
+ *
  * @author Sebastiaan Heijmann
  */
-public class TimeCategoryNode extends BeanNode{
+public class TimeCategoryNode extends BeanNode {
 
-	public TimeCategoryNode(TimeCategory bean) throws IntrospectionException {
-		super(bean);
-		setDisplayName(bean.getIdentifier());
-		setShortDescription(bean.getDescription());
-	}
+    public TimeCategoryNode(TimeCategory bean) throws IntrospectionException {
+        super(bean);
+        setDisplayName(bean.getIdentifier());
+        setShortDescription(bean.getDescription());
+    }
 
-	@Override
-	public Image getIcon(int type) {
-		try {
-			return ImageIO.read(getClass().getClassLoader().getResource("nl/fontys/sofa/limo/view/images/icons/time01-24x24.png"));
-		} catch (IOException ex) {
-			Exceptions.printStackTrace(ex);
-		}
-		return super.getIcon(type);
-	}
-	
+    @Override
+    public Image getIcon(int type) {
+        Image icon = IconUtil.getIcon(TimeCategory.class, type);
+        if (icon == null) {
+            return super.getIcon(type);
+        } else {
+            return icon;
+        }
+    }
+
 }
