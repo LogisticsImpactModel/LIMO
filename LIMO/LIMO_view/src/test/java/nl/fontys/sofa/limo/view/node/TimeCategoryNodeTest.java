59ee48426e618d2046e99542965692b6c3e8a1d4
2194479 <2194479@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-10-10 13:51:41 +0000

    HubTypeNodeTest, LegTypeNodeTest and TimeCategoryNodeTest w sufficient coverage

--- LIMO/LIMO_view/src/test/java/nl/fontys/sofa/limo/view/node/TimeCategoryNodeTest.java
+++ LIMO/LIMO_view/src/test/java/nl/fontys/sofa/limo/view/node/TimeCategoryNodeTest.java
@@ -12,8 +12,8 @@ import java.beans.IntrospectionException;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.Action;
-import nl.fontys.sofa.limo.domain.category.CostCategory;
+import nl.fontys.sofa.limo.domain.category.TimeCategory;
+import nl.fontys.sofa.limo.domain.types.HubType;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -22,15 +22,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.openide.util.Exceptions;
-import org.openide.util.datatransfer.NewType;
 
 /**
  *
  * @author Ben
  */
-public class CostCategoryRootNodeTest {
+public class TimeCategoryNodeTest {
     
-    public CostCategoryRootNodeTest() {
+    public TimeCategoryNodeTest() {
     }
     
     @BeforeClass
@@ -50,44 +49,29 @@ public class CostCategoryRootNodeTest {
     }
 
     /**
-     * Test of getIcon method, of class CostCategoryRootNode.
+     * Test of getIcon method, of class TimeCategoryNode.
      */
     @Test
     public void testGetIcon() {
         System.out.println("getIcon");
         try {
-            CostCategoryNode ccn = new CostCategoryNode(new CostCategory());
+            TimeCategoryNode tcn = new TimeCategoryNode(new TimeCategory());
             try {
                 //load orig. icon what we are looking for for comparison
-                BufferedImage sourceImg = ImageIO.read(new File("src/main/resources/icons/CostCategory_32x32.png"));
+                BufferedImage sourceImg = ImageIO.read(new File("src/main/resources/icons/TimeCategory_32x32.png"));
                 //get appl. icon in CostCategoryNode class w/ type 2 (32x32)
-                BufferedImage buffImg = (BufferedImage) ccn.getIcon(2);
+                BufferedImage buffImg = (BufferedImage) tcn.getIcon(2);
                 //compare orig. icon and icon from CCNode's byte[]s
                 Assert.assertArrayEquals("Images should consist out of identical byteArrays",((DataBufferByte) sourceImg.getData().getDataBuffer()).getData(),((DataBufferByte) buffImg.getData().getDataBuffer()).getData());
             } catch (IOException ex) {
                 Exceptions.printStackTrace(ex);
+                fail("Could not testGetIcon");
             }
         } catch (IntrospectionException ex) {
             Exceptions.printStackTrace(ex);
+            fail("Could not testGetIcon");
         }  
-    }
-
-    /**
-     * Test of getActions method, of class CostCategoryRootNode.
-     */
-    @Test
-    public void testGetActions() {
-        System.out.println("getActions");
-      
-    }
-
-    /**
-     * Test of getNewTypes method, of class CostCategoryRootNode.
-     */
-    @Test
-    public void testGetNewTypes() {
-        System.out.println("getNewTypes");
-        
+    
     }
     
 }
