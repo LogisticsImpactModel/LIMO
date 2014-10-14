076677d37560e56f00d441b4ffc640e807e9a7d7
2192656 <2192656@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-10-14 09:14:49 +0000

    - Added ProcessResponsibilityDirection

---
+++ LIMO/LIMO-domain/src/main/java/nl/fontys/sofa/limo/domain/component/process/ProcessResponsibilityDirection.java
@@ -0,0 +1,13 @@
+package nl.fontys.sofa.limo.domain.component.process;
+
+/**
+ * Shows whether a process belongs to the input or output actor.
+ *
+ * @author Matthias Brück
+ */
+public enum ProcessResponsibilityDirection {
+
+    INPUT,
+    OUTPUT,
+    BOTH;
+}
