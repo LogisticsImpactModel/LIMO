013bdfa5171fed4d849f074f004a785716bd7f0b
2192656 <2192656@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-10-17 10:17:14 +0000

    - Added Procedure Component Panel -- Edit Procedure -- Add Procedure -- Delete Procedure -- Added "Test" to play around

--- LIMO/LIMO_view/src/test/java/nl/fontys/sofa/limo/view/custom/table/DragNDropTest.java
+++ LIMO/LIMO_view/src/test/java/nl/fontys/sofa/limo/view/custom/table/DragNDropTest.java
@@ -5,6 +5,9 @@
  */
 package nl.fontys.sofa.limo.view.custom.table;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.DropMode;
 import javax.swing.JFrame;
 
@@ -21,8 +24,29 @@ public class DragNDropTest {
         frame = new JFrame("Drag 'n Drop Test");
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setSize(800, 450);
+        List<List<Object>> values = new ArrayList<>();
+        ArrayList<Object> value = new ArrayList<>();
+        value.add("A");
+        value.add("B");
+        values.add(value);
+        value = new ArrayList<>();
+        value.add("C");
+        value.add("D");
+        values.add(value);
+        value = new ArrayList<>();
+        value.add("E");
+        value.add("F");
+        values.add(value);
+        value = new ArrayList<>();
+        value.add("G");
+        value.add("H");
+        values.add(value);
+        value = new ArrayList<>();
+        value.add("I");
+        value.add("J");
+        values.add(value);
         DragNDropTableModel model = new DragNDropTableModel(new String[]{"C1", "C2"},
-                new String[][]{{"A", "B"}, {"C", "D"}, {"E", "F"}, {"G", "H"}, {"I", "J"}},
+                values,
                 new Class[]{String.class, String.class});
         table = new DragNDropTable(model);
         table.getColumnModel().getColumn(0).setPreferredWidth(300);
