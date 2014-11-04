af88a47cc4ea8c9822468ebb750fd8c0cfd0d983
2042635 <2042635@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-11-04 13:46:36 +0000

    implement delete action

--- LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/factory/HubTypeChildFactory.java
+++ LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/factory/HubTypeChildFactory.java
@@ -1,6 +1,8 @@
 package nl.fontys.sofa.limo.view.factory;
 
 import java.beans.IntrospectionException;
+import java.beans.PropertyChangeEvent;
+import java.util.Collection;
 import java.util.List;
 import nl.fontys.sofa.limo.api.service.provider.HubTypeService;
 import nl.fontys.sofa.limo.domain.component.type.HubType;
@@ -8,22 +10,28 @@ import nl.fontys.sofa.limo.view.node.HubTypeNode;
 import org.openide.nodes.BeanNode;
 import org.openide.nodes.ChildFactory;
 import org.openide.nodes.Node;
+import org.openide.nodes.NodeEvent;
+import org.openide.nodes.NodeListener;
+import org.openide.nodes.NodeMemberEvent;
+import org.openide.nodes.NodeReorderEvent;
 import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 import org.openide.util.Lookup.Result;
 import org.openide.util.LookupEvent;
 import org.openide.util.LookupListener;
+import org.openide.util.Utilities;
 
 /**
- * Factory responsible for creating the HubType children.
+ * Factory responsible for creating the HubType children. It listens
+ * to changes in the service layer and in the nodes.
  *
  * @author Sebastiaan Heijmann
  */
 public class HubTypeChildFactory extends ChildFactory<HubType> 
-		implements LookupListener{
+		implements LookupListener, NodeListener{
 
 	private final Result<HubType> lookupResult;
-	private HubTypeService service; 
+	private final HubTypeService service; 
 
 	public HubTypeChildFactory() {
 		service = Lookup.getDefault().lookup(HubTypeService.class);
@@ -52,5 +60,30 @@ public class HubTypeChildFactory extends ChildFactory<HubType>
 	public void resultChanged(LookupEvent le) {
 		refresh(true);
 	}
+
+			@Override
+	public void nodeDestroyed(NodeEvent ne) {
+		Node node = ne.getNode();
+		HubType ht =
+				(HubType) node.getLookup().lookup(HubType.class);
+
+		Lookup.Result result = Utilities.actionsGlobalContext().lookupResult (HubType.class);
+		Collection<HubType> selectedBeans = result.allInstances();
+		for(HubType bean : selectedBeans){
+			if(bean == ht){
+				service.delete(ht);
+			}	
+		}
+		refresh(true);
+	}
+
+	@Override
+	public void childrenAdded(NodeMemberEvent ev) {}
+	@Override
+	public void childrenRemoved(NodeMemberEvent ev) {}
+	@Override
+	public void childrenReordered(NodeReorderEvent ev) {}
+	@Override
+	public void propertyChange(PropertyChangeEvent pce) {}
 
 }
