6d1f6c62282e589a42af343621e2f8e52e27ae55
2192656 <2192656@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-11-04 09:20:27 +0000

    - Better

--- LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/custom/ProcedureComponent.java
+++ LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/custom/ProcedureComponent.java
@@ -3,10 +3,15 @@ package nl.fontys.sofa.limo.view.custom;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.DefaultCellEditor;
@@ -27,6 +32,7 @@ import nl.fontys.sofa.limo.domain.component.procedure.value.SingleValue;
 import nl.fontys.sofa.limo.domain.component.procedure.value.Value;
 import nl.fontys.sofa.limo.view.custom.table.DragNDropTable;
 import nl.fontys.sofa.limo.view.custom.table.DragNDropTableModel;
+import org.apache.commons.math3.genetics.ElitisticListPopulation;
 import org.openide.util.Lookup;
 
 /**
@@ -249,9 +255,14 @@ public class ProcedureComponent extends JPanel implements ActionListener, MouseL
             btn_addTime.addActionListener(this);
             //DIALOG OPTIONS
             this.setSize(250, 300);
-            this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
             this.setModal(true);
             this.setAlwaysOnTop(true);
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            Dimension screenSize = toolkit.getScreenSize();
+            int x = (screenSize.width - this.getWidth()) / 2;
+            int y = (screenSize.height - this.getHeight()) / 2;
+            this.setLocation(x, y);
+            this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
             this.setVisible(true);
         }
 
@@ -382,8 +393,13 @@ public class ProcedureComponent extends JPanel implements ActionListener, MouseL
             //DIALOG OPTIONS
             this.setModal(true);
             this.setSize(200, 250);
-            this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
             this.setAlwaysOnTop(true);
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            Dimension screenSize = toolkit.getScreenSize();
+            int x = (screenSize.width - this.getWidth()) / 2;
+            int y = (screenSize.height - this.getHeight()) / 2;
+            this.setLocation(x, y);
+            this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
             this.setVisible(true);
         }
 
