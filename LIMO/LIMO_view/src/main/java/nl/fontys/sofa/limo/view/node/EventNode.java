70745ae1d0549268cff94b214f353cf377a30bf6
2184008 <2184008@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-11-05 21:59:16 +0000

    commit changes

--- LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/node/EventNode.java
+++ LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/node/EventNode.java
@@ -1,20 +1,41 @@
 package nl.fontys.sofa.limo.view.node;
 
+import java.awt.event.ActionEvent;
 import java.beans.IntrospectionException;
-import nl.fontys.sofa.limo.domain.BaseEntity;
+import java.util.Arrays;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import nl.fontys.sofa.limo.domain.component.event.Event;
+import nl.fontys.sofa.limo.view.wizard.event.EventWizardAction;
 
-/**
- *
- * @author Sebastiaan Heijmann
- */
 public class EventNode extends AbstractBeanNode {
 
-    public EventNode(BaseEntity bean) throws IntrospectionException {
-        super(bean);
+    private final Event event;
+
+    public EventNode(Event event) throws IntrospectionException {
+        super(event);
+        this.event = event;
     }
 
     @Override
     public boolean canDestroy() {
         return true;
     }
+
+    @Override
+    public Action[] getActions(boolean context) {
+        Action[] actions = super.getActions(context);
+        Action editAction = new AbstractAction("Edit") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                EventWizardAction wiz = new EventWizardAction();
+                wiz.isUpdate(event);
+                wiz.actionPerformed(e);
+            }
+        };
+        Action[] result = Arrays.copyOf(actions, actions.length + 1);
+        result[actions.length] = editAction;
+        return result;
+    }
+
 }
