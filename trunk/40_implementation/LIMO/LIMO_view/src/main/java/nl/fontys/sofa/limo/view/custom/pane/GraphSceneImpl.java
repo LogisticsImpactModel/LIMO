7d14e95e13585d7ba8b6ed6d335d39980d1b2b1a
2042635 <2042635@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-11-07 13:20:20 +0000

    implement actions for graphscene

---
+++ trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/custom/pane/GraphSceneImpl.java
@@ -0,0 +1,239 @@
+package nl.fontys.sofa.limo.view.custom.pane;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.datatransfer.Transferable;
+import java.awt.geom.AffineTransform;
+import java.beans.BeanInfo;
+import javax.swing.JComponent;
+import nl.fontys.sofa.limo.view.factory.WidgetFactory;
+import nl.fontys.sofa.limo.view.node.AbstractBeanNode;
+import nl.fontys.sofa.limo.view.node.ContainerNode;
+import nl.fontys.sofa.limo.view.node.HubNode;
+import org.netbeans.api.visual.action.AcceptProvider;
+import org.netbeans.api.visual.action.ActionFactory;
+import org.netbeans.api.visual.action.ConnectProvider;
+import org.netbeans.api.visual.action.ConnectorState;
+import org.netbeans.api.visual.action.ReconnectProvider;
+import org.netbeans.api.visual.action.WidgetAction;
+import org.netbeans.api.visual.anchor.AnchorFactory;
+import org.netbeans.api.visual.anchor.AnchorShape;
+import org.netbeans.api.visual.anchor.PointShape;
+import org.netbeans.api.visual.graph.GraphScene;
+import org.netbeans.api.visual.widget.ConnectionWidget;
+import org.netbeans.api.visual.widget.LayerWidget;
+import org.netbeans.api.visual.widget.Scene;
+import org.netbeans.api.visual.widget.Widget;
+import org.openide.nodes.Node;
+import org.openide.nodes.NodeTransfer;
+
+/**
+ * GraphScene to draw a chain on. Widgets can be added to this scene to build
+ * the chain from.
+ *
+ * @author Sebastiaan Heijmann
+ */
+public class GraphSceneImpl extends GraphScene<ContainerNode, String> {
+
+	// The different Layers to draw on.
+	private final LayerWidget mainLayer = new LayerWidget(this);
+	private final LayerWidget connectionLayer = new LayerWidget(this);
+	private final LayerWidget interactionLayer = new LayerWidget(this);
+
+	// The actions to be invoked.
+	private final WidgetAction moveAlignAction;
+	private final WidgetAction zoomAction
+			= ActionFactory.createZoomAction();
+	private final WidgetAction panAction
+			= ActionFactory.createPanAction();
+	private final WidgetAction acceptAction
+			= ActionFactory.createAcceptAction(
+					new SceneAcceptProvider());
+	private final WidgetAction connectAction
+			= ActionFactory.createExtendedConnectAction(
+					interactionLayer,
+					new SceneConnectProvider());
+	private final WidgetAction reconnectAction
+			= ActionFactory.createReconnectAction(
+					new SceneReconnectProvider());
+
+	// Variables.
+	private long edgeCounter = 0;
+	private int pos = 0;
+
+	/**
+	 * Constructor sets up the different layers and creates basic actions which
+	 * can be invoked on the scene.
+	 */
+	public GraphSceneImpl() {
+		addChild(mainLayer);
+		addChild(connectionLayer);
+		addChild(interactionLayer);
+		moveAlignAction = ActionFactory.createAlignWithMoveAction(mainLayer, interactionLayer, null);
+		getActions().addAction(zoomAction);
+		getActions().addAction(panAction);
+		getActions().addAction(acceptAction);
+	}
+
+	@Override
+	protected Widget attachNodeWidget(ContainerNode node) {
+		Widget widget = WidgetFactory.createWidgetFromContainerNode(this, node);
+		widget.getActions().addAction(createSelectAction());
+		widget.getActions().addAction(createObjectHoverAction());
+		widget.getActions().addAction(connectAction);
+		widget.getActions().addAction(moveAlignAction);
+		mainLayer.addChild(widget);
+		return widget;
+	}
+
+	@Override
+	protected Widget attachEdgeWidget(String edge) {
+		ConnectionWidget connection = new ConnectionWidget(this);
+		connection.setTargetAnchorShape(AnchorShape.TRIANGLE_FILLED);
+		connection.setEndPointShape(PointShape.SQUARE_FILLED_BIG);
+		connection.getActions().addAction(createObjectHoverAction());
+		connection.getActions().addAction(createSelectAction());
+		connection.getActions().addAction(reconnectAction);
+		connectionLayer.addChild(connection);
+		return connection;
+	}
+
+	@Override
+	protected void attachEdgeSourceAnchor(String edge, ContainerNode oldSourceNode, ContainerNode sourceNode) {
+		Widget w = sourceNode != null ? findWidget(sourceNode) : null;
+		((ConnectionWidget) findWidget(edge)).setSourceAnchor(AnchorFactory.createRectangularAnchor(w));
+	}
+
+	@Override
+	protected void attachEdgeTargetAnchor(String edge, ContainerNode oldTargetNode, ContainerNode targetNode) {
+		Widget w = targetNode != null ? findWidget(targetNode) : null;
+		((ConnectionWidget) findWidget(edge)).setTargetAnchor(AnchorFactory.createRectangularAnchor(w));
+	}
+
+	/**
+	 * Accept provider for the scene. Validates if a connection can be dropped
+	 * and places the connection in the scene.
+	 */
+	private class SceneAcceptProvider implements AcceptProvider {
+
+		@Override
+		public ConnectorState isAcceptable(Widget widget, Point point, Transferable t) {
+			Node node = NodeTransfer.node(t, NodeTransfer.DND_COPY_OR_MOVE);
+			Image dragImage = node.getIcon(BeanInfo.ICON_COLOR_32x32);
+			JComponent view = getView();
+			Graphics2D g2 = (Graphics2D) view.getGraphics();
+			Rectangle visRect = view.getVisibleRect();
+			view.paintImmediately(visRect.x, visRect.y, visRect.width, visRect.height);
+			if (node instanceof HubNode) {
+				g2.drawImage(dragImage,
+						AffineTransform.getTranslateInstance(point.getLocation().getX(),
+								point.getLocation().getY()),
+						null);
+				return ConnectorState.ACCEPT;
+			}
+			return ConnectorState.REJECT;
+		}
+
+		@Override
+		public void accept(Widget widget, Point point, Transferable t) {
+			AbstractBeanNode node = (AbstractBeanNode) NodeTransfer.node(
+					t, NodeTransfer.DND_COPY_OR_MOVE);
+			Widget w = GraphSceneImpl.this.addNode(new ContainerNode(node));
+			w.setPreferredLocation(widget.convertLocalToScene(point));
+		}
+	}
+
+	private class SceneConnectProvider implements ConnectProvider {
+
+		private ContainerNode source = null;
+		private ContainerNode target = null;
+
+		public boolean isSourceWidget(Widget sourceWidget) {
+			Object object = findObject(sourceWidget);
+			source = isNode(object) ? (ContainerNode) object : null;
+			return source != null;
+		}
+
+		public boolean hasCustomTargetWidgetResolver(Scene scene) {
+			return false;
+		}
+
+		public Widget resolveTargetWidget(Scene scene, Point sceneLocation) {
+			return null;
+		}
+
+		public void createConnection(Widget sourceWidget, Widget targetWidget) {
+			String edge = "edge" + edgeCounter++;
+			addEdge(edge);
+			setEdgeSource(edge, source);
+			setEdgeTarget(edge, target);
+		}
+
+		public ConnectorState isTargetWidget(Widget sourceWidget, Widget targetWidget) {
+			Object object = findObject(targetWidget);
+			target = isNode(object) ? (ContainerNode) object : null;
+			if (target != null) {
+				return !source.equals(target) ? ConnectorState.ACCEPT : ConnectorState.REJECT_AND_STOP;
+			}
+			return object != null ? ConnectorState.REJECT_AND_STOP : ConnectorState.REJECT;
+		}
+
+	}
+
+	private class SceneReconnectProvider implements ReconnectProvider {
+
+		String edge;
+		ContainerNode originalNode;
+		ContainerNode replacementNode;
+
+		public void reconnectingStarted(ConnectionWidget connectionWidget, boolean reconnectingSource) {
+		}
+
+		public void reconnectingFinished(ConnectionWidget connectionWidget, boolean reconnectingSource) {
+		}
+
+		public boolean isSourceReconnectable(ConnectionWidget connectionWidget) {
+			Object object = findObject(connectionWidget);
+			edge = isEdge(object) ? (String) object : null;
+			originalNode = edge != null ? getEdgeSource(edge) : null;
+			return originalNode != null;
+		}
+
+		public boolean isTargetReconnectable(ConnectionWidget connectionWidget) {
+			Object object = findObject(connectionWidget);
+			edge = isEdge(object) ? (String) object : null;
+			originalNode = edge != null ? getEdgeTarget(edge) : null;
+			return originalNode != null;
+		}
+
+		public boolean hasCustomReplacementWidgetResolver(Scene scene) {
+			return false;
+		}
+
+		public Widget resolveReplacementWidget(Scene scene, Point sceneLocation) {
+			return null;
+		}
+
+		public void reconnect(ConnectionWidget connectionWidget, Widget replacementWidget, boolean reconnectingSource) {
+			if (replacementWidget == null) {
+				removeEdge(edge);
+			} else if (reconnectingSource) {
+				setEdgeSource(edge, replacementNode);
+			} else {
+				setEdgeTarget(edge, replacementNode);
+			}
+		}
+
+		public ConnectorState isReplacementWidget(ConnectionWidget connectionWidget, Widget replacementWidget, boolean b) {
+			Object object = findObject(replacementWidget);
+			replacementNode = isNode(object) ? (ContainerNode) object : null;
+			if (replacementNode != null) {
+				return ConnectorState.ACCEPT;
+			}
+			return object != null ? ConnectorState.REJECT_AND_STOP : ConnectorState.REJECT;
+		}
+
+	}
+}
--- trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/widget/HubWidget.java
+++ trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/widget/HubWidget.java
@@ -1,8 +1,19 @@
 package nl.fontys.sofa.limo.view.widget;
 
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.util.List;
+import javax.swing.AbstractAction;
+import javax.swing.JPopupMenu;
+import nl.fontys.sofa.limo.view.custom.pane.ChainScene;
 import nl.fontys.sofa.limo.view.node.ContainerNode;
+import org.netbeans.api.visual.action.ActionFactory;
+import org.netbeans.api.visual.action.PopupMenuProvider;
+import org.netbeans.api.visual.widget.LayerWidget;
 import org.netbeans.api.visual.widget.Scene;
+import org.netbeans.api.visual.widget.Widget;
 import org.netbeans.api.visual.widget.general.IconNodeWidget;
+import org.openide.util.lookup.Lookups;
 
 /**
  * Widget which holds a ContainerNode containing a HubNode. This Widget can be
@@ -11,6 +22,7 @@ import org.netbeans.api.visual.widget.general.IconNodeWidget;
  * @author Sebastiaan Heijmann
  */
 public class HubWidget extends IconNodeWidget{
+	private ContainerNode container;
 
 	/**
 	 * Constructor sets up the widget by setting the display name and image.
@@ -19,7 +31,29 @@ public class HubWidget extends IconNodeWidget{
 	 */
 	public HubWidget(Scene scene, ContainerNode container) {
 		super(scene);
+		setImage(container.getImage());
+		setLabel(container.getDisplayName());
+		this.container = container;
+//		getActions().addAction(ActionFactory.createPopupMenuAction(new WidgetPopupMenu()));
 	}
+
+//	private class WidgetPopupMenu implements PopupMenuProvider{
+//
+//		@Override
+//		public JPopupMenu getPopupMenu(Widget widget, Point localLocation) {
+//			JPopupMenu popup =  new JPopupMenu();
+//			popup.add(new AbstractAction("Delete") {
+//				
+//				@Override
+//				public void actionPerformed(ActionEvent ae) {
+//					ChainScene scene = (ChainScene) getScene();
+//					scene.removeNodeWithAttachedEdges(container);
+//				}
+//			});
+//			return popup;
+//		}
+//
+//	}
 
 
 }
