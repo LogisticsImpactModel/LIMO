471c1c8b2f64daf7c28e63608ef08f6baf6c9ba0
2192656 <2192656@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-11-13 08:42:19 +0000

    -HubTypeNode --Added Property Editors for name, description, icon -HubNode --Added Property Editors for name, description, icon --Changed HubRootNode for correct display

--- trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/node/HubTypeNode.java
+++ trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/node/HubTypeNode.java
@@ -1,14 +1,20 @@
 package nl.fontys.sofa.limo.view.node;
 
+import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
+import javax.swing.ImageIcon;
+import nl.fontys.sofa.limo.domain.component.Icon;
 import nl.fontys.sofa.limo.domain.component.type.HubType;
+import nl.fontys.sofa.limo.view.node.property.StupidProperty;
+import org.openide.ErrorManager;
+import org.openide.nodes.Sheet;
 
 /**
  * View representation of HubType.
  *
  * @author Sebastiaan Heijmann
  */
-public class HubTypeNode extends AbstractBeanNode {
+public class HubTypeNode extends AbstractBeanNode<HubType> {
 
     public HubTypeNode(HubType bean) throws IntrospectionException {
         super(bean, HubType.class);
@@ -18,5 +24,38 @@ public class HubTypeNode extends AbstractBeanNode {
     public boolean canDestroy() {
         return true;
     }
-
+    
+    @Override
+    protected void createProperties(HubType bean, BeanInfo info) {
+        //super.createProperties(bean, info);
+        
+        Sheet sets = getSheet();
+        Sheet.Set set = Sheet.createPropertiesSet();
+        
+        try {
+            StupidProperty name = new StupidProperty<>(getBean(), String.class, "name");
+            name.addPropertyChangeListener(getListener());
+            name.setDisplayName("Name");
+            name.setShortDescription("The name of the procedure category.");
+            
+            StupidProperty description = new StupidProperty<>(getBean(), String.class, "description");
+            description.addPropertyChangeListener(getListener());
+            description.setDisplayName("Description");
+            description.setShortDescription("An optional short description of the procedure category.");
+            
+            StupidProperty iconProp = new StupidProperty(getBean(), Icon.class, "icon");
+            iconProp.addPropertyChangeListener(getListener());
+            iconProp.setDisplayName("Icon");
+            iconProp.setShortDescription("The icon that gets displayed with this Leg-Type.");
+            iconProp.setValue("valueIcon", new ImageIcon(getBean().getIcon().getImage()));
+            iconProp.setValue("canEditAsText", false);
+            
+            set.put(name);
+            set.put(description);
+            set.put(iconProp);
+        } catch (NoSuchMethodException ex) {
+            ErrorManager.getDefault();
+        }
+        sets.put(set);
+    }
 }
