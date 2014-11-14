01692b3ebf2e0aafbf0203ae929edff411ef346d
2192656 <2192656@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-11-14 13:29:43 +0000

    changing name when updated

--- LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/node/EventRootNode.java
+++ LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/node/EventRootNode.java
@@ -1,9 +1,14 @@
 package nl.fontys.sofa.limo.view.node;
 
+import java.io.IOException;
 import nl.fontys.sofa.limo.api.exception.ServiceNotFoundException;
 import nl.fontys.sofa.limo.api.service.provider.EventService;
 import nl.fontys.sofa.limo.domain.component.event.Event;
+import nl.fontys.sofa.limo.view.custom.pane.NameDescriptionDialogInputPane;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
 import org.openide.nodes.Children;
+import org.openide.util.datatransfer.NewType;
 
 /**
  *
@@ -23,6 +28,34 @@ public class EventRootNode extends AbstractRootNode {
     @Override
     Class getBeanClass() {
         return Event.class;
+    }
+    
+    @Override
+    public NewType[] getNewTypes() {
+        return new NewType[]{new NewType() {
+
+            @Override
+            public String getName() {
+                return "Hub";
+            }
+
+            @Override
+            public void create() throws IOException {
+                NameDescriptionDialogInputPane inputPane = new NameDescriptionDialogInputPane();
+                DialogDescriptor dd = new DialogDescriptor(inputPane, "Event");
+                Object result = DialogDisplayer.getDefault().notify(dd);
+
+                String name = inputPane.getNameFieldValue();
+                String description = inputPane.getDescriptionFieldValue();
+
+                if (!name.isEmpty() && !description.isEmpty()) {
+                    Event e = new Event();
+                    e.setName(name);
+                    e.setDescription(description);
+                    service.insert(e);
+                }
+            }
+        }};
     }
 
 }
