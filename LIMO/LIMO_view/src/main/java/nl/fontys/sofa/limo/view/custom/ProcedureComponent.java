7ef8636fcb7bb7e4e3822928addd2eec9b9a4b4e
2184008 <2184008@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-11-18 12:11:01 +0000

    update procedure component

--- LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/custom/ProcedureComponent.java
+++ LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/custom/ProcedureComponent.java
@@ -2,9 +2,6 @@ package nl.fontys.sofa.limo.view.custom;
 
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -15,19 +12,16 @@ import javax.swing.DefaultCellEditor;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextField;
 import nl.fontys.sofa.limo.api.dao.ProcedureCategoryDAO;
 import nl.fontys.sofa.limo.domain.component.procedure.Procedure;
 import nl.fontys.sofa.limo.domain.component.procedure.ProcedureCategory;
 import nl.fontys.sofa.limo.domain.component.procedure.ProcedureResponsibilityDirection;
 import nl.fontys.sofa.limo.domain.component.procedure.TimeType;
-import nl.fontys.sofa.limo.domain.component.procedure.value.RangeValue;
-import nl.fontys.sofa.limo.domain.component.procedure.value.SingleValue;
 import nl.fontys.sofa.limo.domain.component.procedure.value.Value;
+import nl.fontys.sofa.limo.view.custom.procedure.AddProcedureDialog;
+import nl.fontys.sofa.limo.view.custom.procedure.EditValueDialog;
 import nl.fontys.sofa.limo.view.custom.table.DragNDropTable;
 import nl.fontys.sofa.limo.view.custom.table.DragNDropTableModel;
 import nl.fontys.sofa.limo.view.util.IconUtil;
@@ -155,7 +149,7 @@ public class ProcedureComponent extends JPanel implements ActionListener, MouseL
     }
 
     private void addProcedure() {
-        new AddProcedureDialog();
+        new AddProcedureDialog(procedureCategoryDao, table);
     }
 
     private void deleteProcedure(int row) {
@@ -167,10 +161,16 @@ public class ProcedureComponent extends JPanel implements ActionListener, MouseL
     private void editProcedure() {
         if (table.getSelectedColumn() == 3 || table.getSelectedColumn() == 4) {
             changedValue = (Value) table.getValueAt(table.getSelectedRow(), table.getSelectedColumn());
-            new EditValueDialog((Value) table.getValueAt(table.getSelectedRow(), table.getSelectedColumn()));
-            table.setValueAt(changedValue, table.getSelectedRow(), table.getSelectedColumn());
-            this.revalidate();
-            this.repaint();
+            new EditValueDialog((Value) table.getValueAt(table.getSelectedRow(), table.getSelectedColumn()), new AddProcedureDialog.EditValueDialogListener() {
+
+                @Override
+                public void newValue(Value value) {
+                    changedValue = value;
+                    table.setValueAt(value, table.getSelectedRow(), table.getSelectedColumn());
+                    ProcedureComponent.this.revalidate();
+                    ProcedureComponent.this.repaint();
+                }
+            });
         }
     }
 
@@ -192,275 +192,4 @@ public class ProcedureComponent extends JPanel implements ActionListener, MouseL
     }
     //</editor-fold>
 
-    private class AddProcedureDialog extends JDialog implements ActionListener {
-
-        private final JButton btn_addSave, btn_addCancel, btn_addTime, btn_addCost;
-        private final JLabel lbl_name, lbl_category, lbl_timeType, lbl_time, lbl_cost, lbl_direction;
-        private final JTextField tf_name, tf_cost, tf_time;
-        private final JComboBox cbox_timeType, cbox_direction;
-        private JComboBox cbox_category;
-        private Value timeValue, costValue;
-        private Procedure newProcedure;
-
-        public AddProcedureDialog() {
-            //LAYOUT
-            FormLayout layout = new FormLayout("5px, pref, 5px, pref, pref:grow, 5px, pref, 5px",
-                    "5px, pref, 5px, pref, 5px, pref, 5px, pref, 5px, pref, 5px, pref, 5px, pref, 5px");
-            this.setLayout(layout);
-            //COMPONENTS
-            lbl_name = new JLabel("Name:");
-            tf_name = new JTextField();
-            lbl_category = new JLabel("Category:");
-            try {
-                cbox_category = new JComboBox(procedureCategoryDao.findAll().toArray());
-            } catch (Exception e) {
-                cbox_category = new JComboBox(new ProcedureCategory[]{});
-            }
-            lbl_timeType = new JLabel("Time Type:");
-            cbox_timeType = new JComboBox(TimeType.values());
-            lbl_time = new JLabel("Time Cost:");
-            timeValue = new SingleValue(0.0);
-            tf_time = new JTextField(timeValue.toString());
-            tf_time.setEditable(false);
-            btn_addTime = new JButton("...");
-            costValue = new SingleValue(0.0);
-            lbl_cost = new JLabel("Money Cost:");
-            tf_cost = new JTextField(costValue.toString());
-            tf_cost.setEditable(false);
-            btn_addCost = new JButton("...");
-            lbl_direction = new JLabel("Direction:");
-            cbox_direction = new JComboBox(ProcedureResponsibilityDirection.values());
-            btn_addSave = new JButton("Save");
-            btn_addCancel = new JButton("Cancel");
-            //ADD COMPONENTS
-            this.add(lbl_name, cc.xy(2, 2));
-            this.add(tf_name, cc.xyw(4, 2, 2));
-            this.add(lbl_category, cc.xy(2, 4));
-            this.add(cbox_category, cc.xyw(4, 4, 2));
-            this.add(lbl_timeType, cc.xy(2, 6));
-            this.add(cbox_timeType, cc.xyw(4, 6, 2));
-            this.add(lbl_time, cc.xy(2, 8));
-            this.add(tf_time, cc.xyw(4, 8, 2));
-            this.add(btn_addTime, cc.xy(7, 8));
-            this.add(lbl_cost, cc.xy(2, 10));
-            this.add(tf_cost, cc.xyw(4, 10, 2));
-            this.add(btn_addCost, cc.xy(7, 10));
-            this.add(lbl_direction, cc.xy(2, 12));
-            this.add(cbox_direction, cc.xyw(4, 12, 2));
-            this.add(btn_addSave, cc.xy(2, 14));
-            this.add(btn_addCancel, cc.xy(4, 14));
-            //ADD COMPONENTS TO LISTENER
-            btn_addCancel.addActionListener(this);
-            btn_addSave.addActionListener(this);
-            btn_addCost.addActionListener(this);
-            btn_addTime.addActionListener(this);
-            //DIALOG OPTIONS
-            this.setSize(250, 300);
-            this.setModal(true);
-            this.setAlwaysOnTop(true);
-            Toolkit toolkit = Toolkit.getDefaultToolkit();
-            Dimension screenSize = toolkit.getScreenSize();
-            int x = (screenSize.width - this.getWidth()) / 2;
-            int y = (screenSize.height - this.getHeight()) / 2;
-            this.setLocation(x, y);
-            this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-            this.setVisible(true);
-        }
-
-        private boolean isValidProcedure() {
-            return !(tf_name.getText().equals("") || tf_name.getText().equals("") || tf_cost.getText().equals(""));
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource().equals(btn_addCost)) {
-                new EditValueDialog(costValue);
-                if (costValue != null) {
-                    costValue = changedValue;
-                    tf_cost.setText(costValue.toString());
-                    this.revalidate();
-                    this.repaint();
-                }
-            }
-            if (e.getSource().equals(btn_addTime)) {
-                new EditValueDialog(timeValue);
-                if (timeValue != null) {
-                    timeValue = changedValue;
-                    tf_time.setText(timeValue.toString());
-                    this.revalidate();
-                    this.repaint();
-                }
-            }
-            if (e.getSource().equals(btn_addCancel)) {
-                this.dispose();
-            }
-            if (e.getSource().equals(btn_addSave)) {
-                if (isValidProcedure()) {
-                    String name = tf_name.getText();
-                    String category = "";
-                    try {
-                        category = cbox_category.getSelectedItem().toString();
-                    } catch (Exception ex) {
-                    }
-                    TimeType timeType = (TimeType) cbox_timeType.getSelectedItem();
-                    ProcedureResponsibilityDirection direction = (ProcedureResponsibilityDirection) cbox_direction.getSelectedItem();
-                    newProcedure = new Procedure(name, category, costValue, timeValue, timeType, direction);
-                    List<Object> newRow = new ArrayList<>();
-                    newRow.add(newProcedure.getName());
-                    newRow.add(newProcedure.getCategory());
-                    newRow.add(newProcedure.getTimeType());
-                    newRow.add(newProcedure.getTime());
-                    newRow.add(newProcedure.getCost());
-                    newRow.add(newProcedure.getDirection());
-                    ((DragNDropTableModel) table.getModel()).addRow(newRow);
-                    ((DragNDropTableModel) table.getModel()).fireTableDataChanged();
-                    table.revalidate();
-                    table.repaint();
-                    this.dispose();
-                }
-            }
-        }
-    }
-
-    private class EditValueDialog extends JDialog implements ActionListener {
-
-        private final JButton btn_dialogSave, btn_dialogCancel;
-        private final JComboBox<String> cbox_valueType;
-        private final JTextField tf_value, tf_min, tf_max;
-        private final JPanel singlePanel, rangePanel;
-        private final JLabel lbl_type, lbl_value, lbl_min, lbl_max, lbl_error;
-        private int activeType = 0;
-
-        public EditValueDialog(Value value) {
-            //LAYOUT
-            FormLayout mainLayout = new FormLayout("5px, pref, 5px, pref, 5px, pref:grow, 5px",
-                    "5px, pref, 5px, pref, 5px, pref, 5px, pref, 5px");
-            FormLayout rangeLayout = new FormLayout("5px, pref, 5px, pref:grow, 5px", "5px, pref, 5px, pref, 5px");
-            FormLayout singleLayout = new FormLayout("5px, pref, 5px, pref:grow, 5px", "5px, pref, 5px");
-            this.setLayout(mainLayout);
-            //COMPONENTS
-            tf_value = new JTextField();
-            tf_min = new JTextField();
-            tf_max = new JTextField();
-            singlePanel = new JPanel();
-            rangePanel = new JPanel();
-            lbl_type = new JLabel("Type: ");
-            lbl_value = new JLabel("Value: ");
-            lbl_min = new JLabel("Min: ");
-            lbl_max = new JLabel("Max: ");
-            lbl_error = new JLabel();
-            lbl_error.setForeground(Color.RED);
-            btn_dialogCancel = new JButton("Cancel");
-            btn_dialogSave = new JButton("Save");
-            cbox_valueType = new JComboBox<>(new String[]{"Single", "Range"});
-            //ADD COMPONENTS TO SINGLE PANEL
-            singlePanel.setLayout(singleLayout);
-            singlePanel.add(lbl_value, cc.xy(2, 2));
-            singlePanel.add(tf_value, cc.xy(4, 2));
-            //ADD COMPONENTS TO RANGE PANEL
-            rangePanel.setLayout(rangeLayout);
-            rangePanel.add(lbl_min, cc.xy(2, 2));
-            rangePanel.add(tf_min, cc.xy(4, 2));
-            rangePanel.add(lbl_max, cc.xy(2, 4));
-            rangePanel.add(tf_max, cc.xy(4, 4));
-            //ADD COMPONENTS TO DIALOG
-            this.add(lbl_type, cc.xy(2, 2));
-            this.add(cbox_valueType, cc.xyw(4, 2, 3));
-            if (value != null) {
-                if (value instanceof SingleValue) {
-                    this.add(singlePanel, cc.xyw(2, 4, 5));
-                    cbox_valueType.setSelectedIndex(0);
-                    tf_value.setText(value.getValue() + "");
-                    activeType = 0;
-                } else {
-                    this.add(rangePanel, cc.xyw(2, 4, 5));
-                    cbox_valueType.setSelectedIndex(1);
-                    tf_min.setText(value.getMin() + "");
-                    tf_max.setText(value.getMax() + "");
-                    activeType = 1;
-                }
-            } else {
-                this.add(singlePanel, cc.xyw(2, 4, 5));
-                cbox_valueType.setSelectedIndex(0);
-                activeType = 0;
-            }
-            this.add(btn_dialogSave, cc.xy(2, 6));
-            this.add(btn_dialogCancel, cc.xy(4, 6));
-            this.add(lbl_error, cc.xyw(2, 8, 5));
-            //ADD COMPONENTS TO LISTENER
-            btn_dialogCancel.addActionListener(this);
-            btn_dialogSave.addActionListener(this);
-            cbox_valueType.addActionListener(this);
-            //DIALOG OPTIONS
-            this.setModal(true);
-            this.setSize(200, 250);
-            this.setAlwaysOnTop(true);
-            Toolkit toolkit = Toolkit.getDefaultToolkit();
-            Dimension screenSize = toolkit.getScreenSize();
-            int x = (screenSize.width - this.getWidth()) / 2;
-            int y = (screenSize.height - this.getHeight()) / 2;
-            this.setLocation(x, y);
-            this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-            this.setVisible(true);
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource().equals(cbox_valueType)) {
-                if (activeType != cbox_valueType.getSelectedIndex()) {
-                    double activeValue;
-                    if (activeType == 0) {
-                        activeValue = 0;
-                        try {
-                            activeValue = Double.parseDouble(tf_value.getText());
-                        } catch (NumberFormatException ex) {
-                        }
-                        this.remove(singlePanel);
-                        this.add(rangePanel, cc.xyw(2, 4, 5));
-                        tf_min.setText(activeValue + "");
-                        lbl_error.setText("");
-                    } else {
-                        activeValue = 0;
-                        try {
-                            activeValue = Double.parseDouble(tf_min.getText());
-                        } catch (NumberFormatException ex) {
-                        }
-                        this.remove(rangePanel);
-                        this.add(singlePanel, cc.xyw(2, 4, 5));
-                        tf_value.setText(activeValue + "");
-                        lbl_error.setText("");
-                    }
-                    activeType = cbox_valueType.getSelectedIndex();
-                    this.revalidate();
-                    this.repaint();
-                }
-            }
-            if (e.getSource().equals(btn_dialogCancel)) {
-                this.dispose();
-            }
-            if (e.getSource().equals(btn_dialogSave)) {
-                if (activeType == 0) {
-                    try {
-                        changedValue = new SingleValue(Double.parseDouble(tf_value.getText()));
-                        this.dispose();
-                    } catch (NumberFormatException ex) {
-                        lbl_error.setText("NOT A NUMBER");
-                    }
-                } else {
-                    try {
-                        double min = Double.parseDouble(tf_min.getText());
-                        double max = Double.parseDouble(tf_max.getText());
-                        if (max > min) {
-                            changedValue = new RangeValue(min, max);
-                            this.dispose();
-                        } else {
-                            lbl_error.setText("MAX MUST BE BIGGER THAN MIN");
-                        }
-                    } catch (NumberFormatException ex) {
-                        lbl_error.setText("NOT A NUMBER");
-                    }
-                }
-            }
-        }
-    }
 }
