306290d7f24e975ef971b55b6758a8ae0c3baa04
2042635 <2042635@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-11-18 12:42:19 +0000

    ...

--- LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/widget/HubWidget.java
+++ LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/widget/HubWidget.java
@@ -5,10 +5,13 @@ import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.JPopupMenu;
 import nl.fontys.sofa.limo.domain.component.hub.Hub;
+import nl.fontys.sofa.limo.view.chain.ChainGraphScene;
 import nl.fontys.sofa.limo.view.chain.GraphSceneImpl;
+import nl.fontys.sofa.limo.view.node.AbstractBeanNode;
 import nl.fontys.sofa.limo.view.node.ContainerNode;
 import org.netbeans.api.visual.action.ActionFactory;
 import org.netbeans.api.visual.action.PopupMenuProvider;
+import org.netbeans.api.visual.graph.GraphScene;
 import org.netbeans.api.visual.widget.Scene;
 import org.netbeans.api.visual.widget.Widget;
 import org.netbeans.api.visual.widget.general.IconNodeWidget;
@@ -19,39 +22,67 @@ import org.netbeans.api.visual.widget.general.IconNodeWidget;
  *
  * @author Sebastiaan Heijmann
  */
-public class HubWidget extends IconNodeWidget{
-	private ContainerNode container;
+public class HubWidget extends IconNodeWidget implements BasicWidget {
 
-	/**
-	 * Constructor sets up the widget by setting the display name and image.
-	 * @param scene - the scene to display the Widget on.
-	 * @param container - the container for the HubNode.
-	 */
-	public HubWidget(Scene scene, ContainerNode container) {
-		super(scene);
-		setImage(container.getImage());
-		setLabel(container.getDisplayName());
-		this.container = container;
-		getActions().addAction(ActionFactory.createPopupMenuAction(new WidgetPopupMenu()));
-	}
+    private ContainerNode container;
 
-	private class WidgetPopupMenu implements PopupMenuProvider{
-		@Override
-		public JPopupMenu getPopupMenu(Widget widget, Point localLocation) {
-			JPopupMenu popup =  new JPopupMenu();
-			popup.add(new AbstractAction("Delete") {
-				
-				@Override
-				public void actionPerformed(ActionEvent ae) {
-					GraphSceneImpl scene = (GraphSceneImpl) getScene();
-					scene.removeNodeWithEdges(container);
-				}
-			});
-			return popup;
-		}
-	}
+    /**
+     * Constructor sets up the widget by setting the display name and image.
+     *
+     * @param scene - the scene to display the Widget on.
+     * @param hubNode - the HubNode.
+     */
+    public HubWidget(Scene scene, AbstractBeanNode hubNode) {
+        super(scene);
+        this.container = new ContainerNode(hubNode);
+        setImage(container.getImage());
+        setLabel(container.getDisplayName());
+        getActions().addAction(ActionFactory.createPopupMenuAction(new WidgetPopupMenu()));
+    }
 
-	public Hub getHub(){
-		return container.getBeanNode().getLookup().lookup(Hub.class);
-	}
+    @Override
+    public void addActions(ChainGraphScene scene) {
+        getActions().addAction(scene.createSelectAction());
+        getActions().addAction(scene.createObjectHoverAction());
+        getActions().addAction(scene.getConnectAction());
+        getActions().addAction(scene.getMoveAlignAction());
+    }
+
+    @Override
+    public boolean isAcceptable(Widget widget, Point point) {
+        if(widget instanceof ChainGraphScene){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void drop(Widget widget, Point point) {
+        this.setPreferredLocation(point);
+        widget.addChild(this);
+    }
+
+    /**
+     * The popup menu when right clicked on this widget.
+     */
+    private class WidgetPopupMenu implements PopupMenuProvider {
+
+        @Override
+        public JPopupMenu getPopupMenu(Widget widget, Point localLocation) {
+            JPopupMenu popup = new JPopupMenu();
+            popup.add(new AbstractAction("Delete") {
+
+                @Override
+                public void actionPerformed(ActionEvent ae) {
+                    GraphSceneImpl scene = (GraphSceneImpl) getScene();
+                    scene.removeNodeWithEdges(container);
+                }
+            });
+            return popup;
+        }
+    }
+
+    public Hub getHub() {
+        return container.getBeanNode().getLookup().lookup(Hub.class);
+    }
 }
