4223031e2c1bfdd3c8fe73f4a5dba41f4d9ea8c8
2042635 <2042635@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-11-21 14:27:54 +0000

    implement connecting of hubs by using legs

--- trunk/40_implementation/LIMO/LIMO-domain/src/main/java/nl/fontys/sofa/limo/domain/component/leg/Leg.java
+++ trunk/40_implementation/LIMO/LIMO-domain/src/main/java/nl/fontys/sofa/limo/domain/component/leg/Leg.java
@@ -1,5 +1,7 @@
 package nl.fontys.sofa.limo.domain.component.leg;
 
+import java.util.HashMap;
+import java.util.Map;
 import nl.fontys.sofa.limo.domain.component.Icon;
 import nl.fontys.sofa.limo.domain.component.Node;
 import nl.fontys.sofa.limo.domain.component.hub.Hub;
@@ -10,7 +12,7 @@ import nl.fontys.sofa.limo.domain.component.hub.Hub;
  * @author Dominik Kaisers <d.kaisers@student.fontys.nl>
  */
 public class Leg extends Node<Hub> {
-    
+
     private Icon icon;
 
     public Leg() {
@@ -23,5 +25,11 @@ public class Leg extends Node<Hub> {
     public void setIcon(Icon icon) {
         this.icon = icon;
     }
-    
+
+    public Map<Leg, Double> getLegs() {
+        Map<Leg, Double> legs = new HashMap();
+        legs.put(this, 100.0);
+        return legs;
+    }
+
 }
--- trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/widget/LegWidget.java
+++ trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/widget/LegWidget.java
@@ -2,8 +2,10 @@ package nl.fontys.sofa.limo.view.widget;
 
 import java.awt.Point;
 import java.awt.event.ActionEvent;
+import java.util.Map;
 import javax.swing.AbstractAction;
 import javax.swing.JPopupMenu;
+import nl.fontys.sofa.limo.domain.component.leg.Leg;
 import nl.fontys.sofa.limo.view.chain.ChainBuilder;
 import nl.fontys.sofa.limo.view.chain.ChainGraphScene;
 import nl.fontys.sofa.limo.view.node.ContainerNode;
@@ -11,7 +13,9 @@ import org.netbeans.api.visual.action.ActionFactory;
 import org.netbeans.api.visual.action.PopupMenuProvider;
 import org.netbeans.api.visual.anchor.AnchorShape;
 import org.netbeans.api.visual.anchor.PointShape;
+import org.netbeans.api.visual.layout.LayoutFactory;
 import org.netbeans.api.visual.widget.ConnectionWidget;
+import org.netbeans.api.visual.widget.ImageWidget;
 import org.netbeans.api.visual.widget.Scene;
 import org.netbeans.api.visual.widget.Widget;
 
@@ -25,8 +29,10 @@ public class LegWidget extends ConnectionWidget implements BasicWidget {
 
     private ContainerNode container;
 
-    public LegWidget(Scene scene) {
+    public LegWidget(Scene scene, ContainerNode container) {
         super(scene);
+        this.container = container;
+        setChildLegWidgets();
         setTargetAnchorShape(AnchorShape.TRIANGLE_FILLED);
         setEndPointShape(PointShape.SQUARE_FILLED_BIG);
     }
@@ -39,9 +45,23 @@ public class LegWidget extends ConnectionWidget implements BasicWidget {
         getActions().addAction(ActionFactory.createPopupMenuAction(new LegWidget.WidgetPopupMenu()));
     }
 
+    private void setChildLegWidgets() {
+        Map<Leg, Double> legs = getLeg().getLegs();
+        for (Map.Entry<Leg, Double> entry : legs.entrySet()) {
+            ImageWidget iw = new ImageWidget(getScene());
+            iw.setImage(entry.getKey().getIcon().getImage());
+            this.setConstraint(iw, LayoutFactory.ConnectionWidgetLayoutAlignment.TOP_RIGHT, 10);
+            this.addChild(iw);
+        }
+    }
+
     @Override
     public boolean drop(ChainGraphScene scene, ChainBuilder builder, Widget widget, Point point) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Leg getLeg() {
+        return container.getBeanNode().getLookup().lookup(Leg.class);
     }
 
     @Override
