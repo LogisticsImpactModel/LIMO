14e4c1fb94235213d3abd494633ea4eb5cc35461
2192656 <2192656@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-11-21 10:27:01 +0000

    - Type is now a component

--- LIMO/LIMO-domain/src/main/java/nl/fontys/sofa/limo/domain/component/type/Type.java
+++ LIMO/LIMO-domain/src/main/java/nl/fontys/sofa/limo/domain/component/type/Type.java
@@ -1,24 +1,17 @@
 package nl.fontys.sofa.limo.domain.component.type;
 
 import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.Embedded;
-import nl.fontys.sofa.limo.domain.BaseEntity;
+import nl.fontys.sofa.limo.domain.component.Component;
 import nl.fontys.sofa.limo.domain.component.Icon;
-import nl.fontys.sofa.limo.domain.component.event.Event;
-import nl.fontys.sofa.limo.domain.component.procedure.Procedure;
 
 /**
  * A type is a template object for either a hub or a leg.
  *
  * @author Dominik Kaisers <d.kaisers@student.fontys.nl>
  */
-public abstract class Type extends BaseEntity {
+public abstract class Type extends Component {
 
-    @Embedded
-    protected List<Event> events;
-    @Embedded
-    protected List<Procedure> procedures;
     @Embedded
     protected Icon icon;
 
@@ -27,27 +20,11 @@ public abstract class Type extends BaseEntity {
         procedures = new ArrayList<>();
     }
 
-    public List<Procedure> getProcedures() {
-        return procedures;
-    }
-
-    public void setProcedures(List<Procedure> procedures) {
-        this.procedures = procedures;
-    }
-
     public Icon getIcon() {
         return icon;
     }
 
     public void setIcon(Icon icon) {
         this.icon = icon;
-    }
-
-    public List<Event> getEvents() {
-        return events;
-    }
-
-    public void setEvents(List<Event> events) {
-        this.events = events;
     }
 }
