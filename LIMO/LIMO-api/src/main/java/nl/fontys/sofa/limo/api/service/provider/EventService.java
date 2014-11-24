cf98700f6b41bc4b91a0ba8629f576e6445b8e1b
2184008 <2184008@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-11-24 19:22:08 +0000

    upload new projects

---
+++ LIMO/LIMO-api/src/main/java/nl/fontys/sofa/limo/api/service/provider/EventService.java
@@ -0,0 +1,16 @@
+package nl.fontys.sofa.limo.api.service.provider;
+
+import java.util.List;
+import nl.fontys.sofa.limo.api.dao.DAO;
+import nl.fontys.sofa.limo.domain.component.event.Event;
+import org.openide.util.Lookup;
+
+/**
+ * Interface which defines service methods for Events and manages a lookup. A
+ * LookupListener can be attached to the LookupResult to listen for changes in
+ * the datamodels.
+
+ * @author Sebastiaan Heijmann
+ */
+public interface EventService extends Lookup.Provider, DAO<Event>{
+}
---
+++ LIMO/LIMO-api/src/main/java/nl/fontys/sofa/limo/api/service/provider/HubService.java
@@ -0,0 +1,17 @@
+package nl.fontys.sofa.limo.api.service.provider;
+
+import java.util.List;
+import nl.fontys.sofa.limo.api.dao.DAO;
+import nl.fontys.sofa.limo.domain.component.hub.Hub;
+import org.openide.util.Lookup;
+
+/**
+ * Interface which defines service methods for Hubs and manages a
+ * lookup. A LookupListener can be attached to the LookupResult to listen for
+ * changes in the datamodels.
+
+ *
+ * @author Sebastiaan Heijmann
+ */
+public interface HubService extends Lookup.Provider, DAO<Hub>{
+}
