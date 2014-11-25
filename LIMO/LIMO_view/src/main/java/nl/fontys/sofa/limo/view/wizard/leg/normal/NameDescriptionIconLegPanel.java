606154e4df0c966acdb2e0c5b9eec77200e0223c
2184008 <2184008@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-11-25 09:03:44 +0000

    icon stuff

--- LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/wizard/leg/normal/NameDescriptionIconLegPanel.java
+++ LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/wizard/leg/normal/NameDescriptionIconLegPanel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.fontys.sofa.limo.view.wizard.leg.normal;
 
 import java.text.MessageFormat;
@@ -17,16 +12,8 @@ import org.openide.util.HelpCtx;
 
 public class NameDescriptionIconLegPanel implements WizardDescriptor.Panel<WizardDescriptor>, WizardDescriptor.ValidatingPanel<WizardDescriptor> {
 
-    /**
-     * The visual component that displays this panel. If you need to access the
-     * component from this class, just use getComponent().
-     */
     private NameDescriptionIconPanel component;
 
-    // Get the visual component for the panel. In this template, the component
-    // is kept separate. This can be more efficient: if the wizard is created
-    // but never displayed, or not all panels are displayed, it is better to
-    // create only those which really need to be visible.
     @Override
     public NameDescriptionIconPanel getComponent() {
         if (component == null) {
@@ -37,20 +24,12 @@ public class NameDescriptionIconLegPanel implements WizardDescriptor.Panel<Wizar
 
     @Override
     public HelpCtx getHelp() {
-        // Show no Help button for this panel:
         return HelpCtx.DEFAULT_HELP;
-        // If you have context help:
-        // return new HelpCtx("help.key.here");
     }
 
     @Override
     public boolean isValid() {
-        // If it is always OK to press Next or Finish, then:
         return true;
-        // If it depends on some condition (form filled out...) and
-        // this condition changes (last form field filled in...) then
-        // use ChangeSupport to implement add/removeChangeListener below.
-        // WizardDescriptor.ERROR/WARNING/INFORMATION_MESSAGE will also be useful.
     }
 
     @Override
