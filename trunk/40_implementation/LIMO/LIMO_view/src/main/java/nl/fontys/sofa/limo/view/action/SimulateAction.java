cac6d430095dd398eb2d1bf7b1cce2d283b9b545
2042635 <2042635@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-12-02 15:04:48 +0000

    implement busy/disabled component when simulating

--- trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/action/SimulateAction.java
+++ trunk/40_implementation/LIMO/LIMO_view/src/main/java/nl/fontys/sofa/limo/view/action/SimulateAction.java
@@ -1,74 +1,86 @@
 package nl.fontys.sofa.limo.view.action;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-import javax.swing.Action;
+import javax.swing.AbstractAction;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import nl.fontys.limo.simulation.Simulator;
-import nl.fontys.limo.simulation.task.Simulation;
 import nl.fontys.sofa.limo.domain.component.SupplyChain;
 import nl.fontys.sofa.limo.view.chain.ChainBuilder;
+import nl.fontys.sofa.limo.view.chain.ChainGraphScene;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
-import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle.Messages;
 import org.openide.util.Utilities;
+import org.openide.util.actions.Presenter;
+import org.openide.windows.TopComponent;
 
 @ActionID(
         category = "Window",
         id = "nl.fontys.sofa.limo.view.action.SimulateAction"
 )
 @ActionRegistration(
-        iconBase = "icons/gui/simulate.png",
+        lazy = false,
+        //        iconBase = "icons/gui/simulate.png",
         displayName = "#CTL_SimulateAction"
 )
 @ActionReferences({
     @ActionReference(path = "Menu/File", position = 20),
-    @ActionReference(path = "Toolbars/File", position = 25),
+//    @ActionReference(path = "Toolbars/File", position = 25),
     @ActionReference(path = "Shortcuts", name = "D-R")
 })
 
 @Messages("CTL_SimulateAction=Simulate")
 
-public final class SimulateAction implements ActionListener {
+public final class SimulateAction extends AbstractAction implements Presenter.Toolbar {
 
     private Image image;
     private SupplyChain supplyChain;
 
-    public SimulateAction() {
-        try {
-            this.image = ImageIO.read(getClass().getResource("/icons/gui/simulate.png"));
-            //putValue(Action.SMALL_ICON, image);
-        } catch (IOException ex) {
-            Exceptions.printStackTrace(ex);
-        }
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         Lookup global = Utilities.actionsGlobalContext();
-        ChainBuilder chainBuilder = global.lookup(ChainBuilder.class);
+        ChainGraphScene scene = global.lookup(ChainGraphScene.class);
 
-        if (chainBuilder != null && chainBuilder.validate()) {
-            supplyChain = chainBuilder.getSupplyChain();
-            Simulator.simulate(256, supplyChain);
-        } else {
-            DialogDisplayer.getDefault().notify(
-                    new NotifyDescriptor.Message(
-                            "Validation of chain failed.\n\nPlease make sure you"
-                            + " have built your chain correctly.",
-                            NotifyDescriptor.WARNING_MESSAGE));
+//        ChainBuilder chainBuilder = global.lookup(ChainBuilder.class);
+        if (scene != null) {
+            TopComponent tc = scene.getParent();
+            ChainBuilder chainBuilder = scene.getChainBuilder();
+            if (chainBuilder != null && chainBuilder.validate()) {
+                supplyChain = chainBuilder.getSupplyChain();
+                tc.makeBusy(true);
+                scene.setEnabled(false);
+                scene.setBackground(Color.LIGHT_GRAY);
+                Simulator.simulate(256, supplyChain);
+            } else {
+                DialogDisplayer.getDefault().notify(
+                        new NotifyDescriptor.Message(
+                                "Validation of chain failed.\n\nPlease make sure you"
+                                + " have built your chain correctly.",
+                                NotifyDescriptor.WARNING_MESSAGE));
+            }
         }
     }
 
     public void setSupplyChain(SupplyChain supplyChain) {
         this.supplyChain = supplyChain;
+    }
+
+    @Override
+    public Component getToolbarPresenter() {
+        JButton button = new JButton(this);
+        button.setIcon(new ImageIcon(getClass().getClassLoader().getResource("icons/gui/simulate.png")));
+        button.setText("Simulate");
+        button.setOpaque(false);
+        button.setBorder(null);
+        return button;
     }
 }
