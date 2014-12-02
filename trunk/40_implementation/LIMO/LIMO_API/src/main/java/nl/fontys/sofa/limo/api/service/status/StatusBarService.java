0aacf6fc8f9dc4ab7853f16e247503f6fec2389f
2178880 <2178880@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-12-02 15:27:34 +0000

    StatusBar API

---
+++ trunk/40_implementation/LIMO/LIMO_API/src/main/java/nl/fontys/sofa/limo/api/service/status/StatusBarService.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.fontys.sofa.limo.api.service.status;
+
+import java.awt.Component;
+
+public interface StatusBarService{
+     public void setMessage(String msg, int statusState);
+     public Component getComponent();
+}
