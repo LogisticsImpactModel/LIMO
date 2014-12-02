3260209c75f5417598c157e0976a008e8cfaf38e
2182242 <2182242@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-12-02 10:17:46 +0000

    - Added Listerner functionality to SimulatorTask

---
+++ LIMO/LIMO_simulation/src/main/java/nl/fontys/limo/simulation/SimulatorTaskListener.java
@@ -0,0 +1,11 @@
+package nl.fontys.limo.simulation;
+
+/**
+ *
+ * @author Dominik Kaisers <d.kaisers@student.fontys.nl>
+ */
+public interface SimulatorTaskListener {
+
+    public void taskFinished(SimulatorTask task);
+
+}
