bb4edb749f5ff2c707b60ada98bef3b4358d6721
2178880 <2178880@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-12-04 12:39:26 +0000

    Statusbar API update

--- LIMO/LIMO_API/src/main/java/nl/fontys/sofa/limo/api/service/status/StatusBarService.java
+++ LIMO/LIMO_API/src/main/java/nl/fontys/sofa/limo/api/service/status/StatusBarService.java
@@ -7,7 +7,18 @@ package nl.fontys.sofa.limo.api.service.status;
 
 import java.awt.Component;
 
-public interface StatusBarService{
-     public void setMessage(String msg,int action ,int statusState);
-     public Component getComponent();
+public interface StatusBarService {
+
+    public static final int STATE_NONE = 0;
+    public static final int STATE_SUCCESS = 1;
+    public static final int STATE_FAIL = 2;
+
+    public static final int ACTION_CREATE = 0;
+    public static final int ACTION_UPDATE = 0;
+    public static final int ACTION_DELETE = 0;
+    public static final int ACTION_FOUND = 0;
+
+    public void setMessage(String msg, int action, int statusState, Exception e);
+
+    public Component getComponent();
 }
