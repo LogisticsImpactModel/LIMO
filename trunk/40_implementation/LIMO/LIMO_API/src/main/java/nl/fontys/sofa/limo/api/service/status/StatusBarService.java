ec1abef692b0f563867f80bfc32ab4b4c08103b7
2178880 <2178880@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-12-05 12:51:56 +0000

    StatusBar update

--- trunk/40_implementation/LIMO/LIMO_API/src/main/java/nl/fontys/sofa/limo/api/service/status/StatusBarService.java
+++ trunk/40_implementation/LIMO/LIMO_API/src/main/java/nl/fontys/sofa/limo/api/service/status/StatusBarService.java
@@ -10,13 +10,15 @@ import java.awt.Component;
 public interface StatusBarService {
 
     public static final int STATE_NONE = 0;
-    public static final int STATE_SUCCESS = 1;
-    public static final int STATE_FAIL = 2;
+    public static final int STATE_ERROR = 1;
+    public static final int STATE_SUCCESS = 2;
+    public static final int STATE_FAIL = 3;
 
-    public static final int ACTION_CREATE = 0;
-    public static final int ACTION_UPDATE = 0;
-    public static final int ACTION_DELETE = 0;
-    public static final int ACTION_FOUND = 0;
+    public static final int ACTION_NULL = 0;
+    public static final int ACTION_CREATE = 1;
+    public static final int ACTION_UPDATE = 2;
+    public static final int ACTION_DELETE = 3;
+    public static final int ACTION_FOUND = 4;
 
     public void setMessage(String msg, int action, int statusState, Exception e);
 
