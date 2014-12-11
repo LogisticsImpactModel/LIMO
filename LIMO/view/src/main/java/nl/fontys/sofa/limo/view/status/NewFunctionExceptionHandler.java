1ebef40b5143747eade845c3925c6bb169183531
2178880 <2178880@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-12-11 10:42:30 +0000

    Error Level

--- LIMO/view/src/main/java/nl/fontys/sofa/limo/view/status/NewFunctionExceptionHandler.java
+++ LIMO/view/src/main/java/nl/fontys/sofa/limo/view/status/NewFunctionExceptionHandler.java
@@ -1,6 +1,7 @@
 package nl.fontys.sofa.limo.view.status;
 
 import java.util.logging.Handler;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import nl.fontys.sofa.limo.api.service.status.StatusBarService;
 import org.openide.util.Lookup;
@@ -11,12 +12,13 @@ public class NewFunctionExceptionHandler extends Handler {
     public void publish(LogRecord record) {
         if (record != null && record.getMessage() != null) {
             // This is an uncaught exception being thrown.
-             Exception e = new Exception("Error", record.getThrown());
-            if (record.getMessage() != null) {
-                e = new Exception(record.getMessage(), record.getThrown());
+            Exception e = new Exception("Error", record.getThrown());
+            if (record.getLevel() == Level.SEVERE || record.getLevel() == Level.FINE) {
+                if (record.getMessage() != null) {
+                    e = new Exception(record.getMessage(), record.getThrown());
+                }
+                Lookup.getDefault().lookup(StatusBarService.class).setMessage(record.getMessage(), StatusBarService.ACTION_NULL, StatusBarService.STATE_ERROR, e);
             }
-            Lookup.getDefault().lookup(StatusBarService.class).setMessage(record.getMessage(), StatusBarService.ACTION_NULL, StatusBarService.STATE_ERROR, e);
-
         }
     }
 
