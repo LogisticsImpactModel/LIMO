88b82df7973d7aa6cbb9ba480ca5ecdf6b4fdfd6
2194479 <2194479@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-12-11 15:16:01 +0000

    improved javadoc

--- trunk/40_implementation/LIMO/domain/src/main/java/nl/fontys/sofa/limo/domain/component/procedure/ProcedureCategory.java
+++ trunk/40_implementation/LIMO/domain/src/main/java/nl/fontys/sofa/limo/domain/component/procedure/ProcedureCategory.java
@@ -3,8 +3,8 @@ package nl.fontys.sofa.limo.domain.component.procedure;
 import nl.fontys.sofa.limo.domain.BaseEntity;
 
 /**
- * Process category used to store possible categories in the database and
- * display them as a selection list for processes.
+ * ProcedureCategory is used to store possible categories of procedures in the
+ * database and use them to group related procedures.
  *
  * @author Dominik Kaisers <d.kaisers@student.fontys.nl>
  */
