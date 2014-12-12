bc15904ba2c41f89fb37d12a6853e671209672df
2194479 <2194479@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-12-12 10:36:04 +0000

    improved javadoc

--- trunk/40_implementation/LIMO/domain/src/main/java/nl/fontys/sofa/limo/domain/component/hub/Continent.java
+++ trunk/40_implementation/LIMO/domain/src/main/java/nl/fontys/sofa/limo/domain/component/hub/Continent.java
@@ -3,6 +3,27 @@ package nl.fontys.sofa.limo.domain.component.hub;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This software is licensed under the Apache 2 license, quoted below.
+ *
+ * Copyright 2013 Stephen Samuel, https://github.com/sksamuel/gaia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Changes made to project default: adding Implements Serializable
+ *
+ * @author SM
+ */
 public enum Continent implements Comparable<Continent> {
 
     Africa("Africa"),
@@ -14,7 +35,7 @@ public enum Continent implements Comparable<Continent> {
     Oceania("Oceania");
 
     private final String name;
-    private List<SerializableCountry> countries;
+    private final List<SerializableCountry> countries;
 
     private Continent(String name) {
         this.name = name;
@@ -22,7 +43,10 @@ public enum Continent implements Comparable<Continent> {
     }
 
     /**
-     * Returns a list of countries that reside in this continent.
+     * Returns a list of countries that reside in this continent (based on the
+     * name which was given to it in the constructor).
+     *
+     * @return a list of countries
      */
     public List<SerializableCountry> getCountries() {
 
