fa2de8b4b5da83a1927658f544240e05e70f6f75
2182242 <2182242@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-12-12 10:45:12 +0000

    - Added javadoc for exec state

--- LIMO/domain/src/main/java/nl/fontys/sofa/limo/domain/component/event/ExecutionState.java
+++ LIMO/domain/src/main/java/nl/fontys/sofa/limo/domain/component/event/ExecutionState.java
@@ -8,31 +8,76 @@ import java.io.Serializable;
  */
 public enum ExecutionState implements Serializable {
 
+    /**
+     * Sub-events can happen independent of their parent's execution state.
+     */
     INDEPENDENT("Independent", "Execution is independent from parent component's execution state."),
+    /**
+     * Event was executed. Sub-events with this dependency can only execute, if
+     * their parent executed.
+     */
     EXECUTED("Executed", "Execution only possible, if parent component was executed."),
+    /**
+     * Event was not executed. Sub-events with this dependency can only execute,
+     * if their parent was not executed.
+     */
     NOT_EXECUTED("Not executed", "Execution only possible, if parent component was not exeuted.");
 
+    /**
+     * Name of the execution state.
+     */
     private final String name;
+
+    /**
+     * Description for the execution state.
+     */
     private final String description;
 
+    /**
+     * Create execution state.
+     *
+     * @param name Name.
+     * @param description Description.
+     */
     private ExecutionState(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
+    /**
+     * Name of the execution state.
+     *
+     * @return Name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Description of the execution state.
+     *
+     * @return Description.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * For ease of use in tables, combo boxes, etc.
+     *
+     * @return Name.
+     */
     @Override
     public String toString() {
         return this.name;
     }
 
+    /**
+     * Get execution state from a boolean value.
+     *
+     * @param isExecuted Boolean.
+     * @return EXECUTED if true, NOT_EXECUTED if false.
+     */
     public static ExecutionState stateFromBool(boolean isExecuted) {
         return isExecuted ? EXECUTED : NOT_EXECUTED;
     }
