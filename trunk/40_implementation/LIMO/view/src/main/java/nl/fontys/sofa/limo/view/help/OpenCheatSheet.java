2095c13933c7ed613cfb9541f5f297cefb689829
2184008 <2184008@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-12-12 13:06:02 +0000

    refactored a little bit more

--- trunk/40_implementation/LIMO/view/src/main/java/nl/fontys/sofa/limo/view/help/OpenCheatSheet.java
+++ trunk/40_implementation/LIMO/view/src/main/java/nl/fontys/sofa/limo/view/help/OpenCheatSheet.java
@@ -1,22 +1,19 @@
 package nl.fontys.sofa.limo.view.help;
 
-import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import nl.fontys.sofa.limo.view.util.FileUtil;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
-import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
 
 /**
- * opens cheat sheet (sheet.pdf) in the systems default PDF viewer
+ * Opens cheat sheet (cheatSheet.pdf) in the systems default PDF viewer.
+ *
+ * @author Sven Mäurer
+ * @author Pascal Lindner
  */
 @ActionID(
         category = "Help",
@@ -34,30 +31,6 @@ public final class OpenCheatSheet implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String pdf = "help/cheatSheet.pdf";
-        if (Desktop.isDesktopSupported()) {
-            FileOutputStream fos = null;
-            try {
-                InputStream jarPdf = getClass().getClassLoader().getResourceAsStream(pdf);
-                File pdfTemp = new File("cheat.pdf");
-                fos = new FileOutputStream(pdfTemp);
-                while (jarPdf.available() > 0) {
-                    fos.write(jarPdf.read());
-                }
-                Desktop.getDesktop().open(pdfTemp);
-            } catch (FileNotFoundException ex) {
-                Exceptions.printStackTrace(ex);
-            } catch (IOException ex) {
-                Exceptions.printStackTrace(ex);
-            } finally {
-                if (fos != null) {
-                    try {
-                        fos.close();
-                    } catch (IOException ex) {
-                        Exceptions.printStackTrace(ex);
-                    }
-                }
-            }
-        }
+        FileUtil.openPDF("help/", "cheatSheet.pdf");
     }
 }
--- trunk/40_implementation/LIMO/view/src/main/java/nl/fontys/sofa/limo/view/help/OpenManual.java
+++ trunk/40_implementation/LIMO/view/src/main/java/nl/fontys/sofa/limo/view/help/OpenManual.java
@@ -1,27 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.fontys.sofa.limo.view.help;
 
-import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import nl.fontys.sofa.limo.view.util.FileUtil;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
-import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
 
 /**
- * Opens the PDF manual (manual.pdf) in the systems default PDF viewer
+ * Opens the PDF manual (manual.pdf) in the systems default PDF viewer.
+ *
+ * @author Sven Mäurer
+ * @author Pascal Lindner
+ *
  */
 @ActionID(
         category = "Help",
@@ -39,30 +32,6 @@ public final class OpenManual implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String pdf = "help/limo_manual.pdf";
-        if (Desktop.isDesktopSupported()) {
-            FileOutputStream fos = null;
-            try {
-                InputStream jarPdf = getClass().getClassLoader().getResourceAsStream(pdf);
-                File pdfTemp = new File("manual.pdf");
-                fos = new FileOutputStream(pdfTemp);
-                while (jarPdf.available() > 0) {
-                    fos.write(jarPdf.read());
-                }
-                Desktop.getDesktop().open(pdfTemp);
-            } catch (FileNotFoundException ex) {
-                Exceptions.printStackTrace(ex);
-            } catch (IOException ex) {
-                Exceptions.printStackTrace(ex);
-            } finally {
-                if (fos != null) {
-                    try {
-                        fos.close();
-                    } catch (IOException ex) {
-                        Exceptions.printStackTrace(ex);
-                    }
-                }
-            }
-        }
+        FileUtil.openPDF("help/", "manual.pdf");
     }
 }
