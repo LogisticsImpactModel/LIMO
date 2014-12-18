eec0cbc7ea4f9eb50a1a5e21c458a3444e7f5044
2184008 <2184008@997e78c6-f0c3-4988-9e7d-9da3c49e7f37>
2014-12-18 12:15:49 +0000

    improve quality

--- LIMO/view/src/main/java/nl/fontys/sofa/limo/view/node/bean/EventNode.java
+++ LIMO/view/src/main/java/nl/fontys/sofa/limo/view/node/bean/EventNode.java
@@ -20,84 +20,105 @@ import org.openide.ErrorManager;
 import org.openide.nodes.Sheet;
 import org.openide.util.Lookup;
 
+/**
+ * View representation of an event that is displayed in the list.
+ *
+ * @author Sven Mäurer
+ */
 public class EventNode extends AbstractBeanNode<Event> {
-    
+
     public EventNode(Event event) throws IntrospectionException {
         super(event, Event.class);
         this.bean = event;
     }
-    
+
     @Override
     public boolean canDestroy() {
         return true;
-        
+
     }
-    
+
     @Override
     public Action[] getActions(boolean context) {
         ArrayList<Action> actionList = new ArrayList<>();
         actionList.add(new AbstractAction(LIMOResourceBundle.getString("EDIT")) {
             @Override
             public void actionPerformed(ActionEvent e) {
-                EventWizardAction wiz = new EventWizardAction();
-                wiz.setEvent(bean);
-                wiz.actionPerformed(e);
-                createProperties(getBean(), null);
-                setSheet(getSheet());
+                editEvent();
             }
+
         });
         actionList.add(new AbstractAction(LIMOResourceBundle.getString("DELETE")) {
-            
+
             @Override
             public void actionPerformed(ActionEvent e) {
-                int reply = JOptionPane.showConfirmDialog(null, LIMOResourceBundle.getString("DELETE_QUESTION", bean.getName()), LIMOResourceBundle.getString("ARE_YOU_SURE"), JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-                if (reply == JOptionPane.YES_OPTION) {
-                    EventService service = Lookup.getDefault().lookup(EventService.class);
-                    service.delete(bean);
-                }
+                deleteEvent();
             }
+
         });
         return actionList.toArray(new Action[actionList.size()]);
     }
-    
+
+    /**
+     * Open an event wizard with the event for editing.
+     */
+    private void editEvent() {
+        EventWizardAction wiz = new EventWizardAction();
+        wiz.setEvent(bean);
+        wiz.actionPerformed(null);
+        createProperties(getBean(), null);
+        setSheet(getSheet());
+    }
+
+    /**
+     * Delete the event form database.
+     */
+    private void deleteEvent() {
+        int reply = JOptionPane.showConfirmDialog(null, LIMOResourceBundle.getString("DELETE_QUESTION", bean.getName()), LIMOResourceBundle.getString("ARE_YOU_SURE"), JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+        if (reply == JOptionPane.YES_OPTION) {
+            EventService service = Lookup.getDefault().lookup(EventService.class);
+            service.delete(bean);
+        }
+    }
+
     @Override
     protected void createProperties(Event bean, BeanInfo info) {
         Sheet sets = getSheet();
         Sheet.Set set = Sheet.createPropertiesSet();
         set.setName("properties");
         set.setDisplayName(LIMOResourceBundle.getString("PROPERTIES"));
-        
+
         try {
             StupidProperty name = new StupidProperty<>(getBean(), String.class, "name");
             name.addPropertyChangeListener(getListener());
             name.setDisplayName(LIMOResourceBundle.getString("NAME"));
             name.setShortDescription(LIMOResourceBundle.getString("NAME_OF", LIMOResourceBundle.getString("EVENT")));
-            
+
             StupidProperty description = new StupidProperty<>(getBean(), String.class, "description");
             description.addPropertyChangeListener(getListener());
             description.setDisplayName(LIMOResourceBundle.getString("DESCRIPTION"));
             description.setShortDescription(LIMOResourceBundle.getString("DESCRIPTION_OF", LIMOResourceBundle.getString("EVENT")));
-            
+
             StupidProperty distributionProp = new StupidProperty(getBean(), Distribution.class, "probability");
             distributionProp.addPropertyChangeListener(getListener());
             distributionProp.setDisplayName(LIMOResourceBundle.getString("PROBABILITY"));
             distributionProp.setShortDescription(LIMOResourceBundle.getString("PROBABILITY_DESC"));
             distributionProp.setValue("canEditAsText", false);
-            
+
             StupidProperty eventProp = new StupidProperty(getBean(), List.class, "events");
             eventProp.addPropertyChangeListener(getListener());
             eventProp.setPropertyEditorClass(EventPropertyEditor.class);
             eventProp.setDisplayName(LIMOResourceBundle.getString("EVENTS"));
             eventProp.setShortDescription(LIMOResourceBundle.getString("EVENTS_OF", LIMOResourceBundle.getString("EVENT")));
             eventProp.setValue("canEditAsText", false);
-            
+
             StupidProperty procedureProp = new StupidProperty(getBean(), List.class, "procedures");
             procedureProp.addPropertyChangeListener(getListener());
             procedureProp.setPropertyEditorClass(ProcedurePropertyEditor.class);
             procedureProp.setDisplayName(LIMOResourceBundle.getString("PROCEDURES"));
             procedureProp.setShortDescription(LIMOResourceBundle.getString("PROCEDURES_OF", LIMOResourceBundle.getString("EVENT")));
             procedureProp.setValue("canEditAsText", false);
-            
+
             set.put(name);
             set.put(description);
             set.put(distributionProp);
@@ -108,15 +129,15 @@ public class EventNode extends AbstractBeanNode<Event> {
         }
         sets.put(set);
     }
-    
+
     @Override
     public AbstractBeanNode getDetachedNodeCopy() {
         throw new UnsupportedOperationException(LIMOResourceBundle.getString("COPY_NOT_SUPPORTED"));
     }
-    
+
     @Override
     Class getServiceClass() {
         return EventService.class;
     }
-    
+
 }
