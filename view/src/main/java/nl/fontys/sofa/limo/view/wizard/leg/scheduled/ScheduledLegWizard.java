c6b6904438f97e5e8e368483ee1d0310a1b2c83d
Merlissa <kikx.ai@home.nl>
2015-04-20 15:38:25 +0200

    LIMO-135 added validation to scheduled leg when clicking next before adding alternative leg

--- view/src/main/java/nl/fontys/sofa/limo/view/wizard/leg/scheduled/ScheduledLegWizard.java
+++ view/src/main/java/nl/fontys/sofa/limo/view/wizard/leg/scheduled/ScheduledLegWizard.java
@@ -2,6 +2,7 @@ package nl.fontys.sofa.limo.view.wizard.leg.scheduled;
 
 import javax.swing.event.ChangeListener;
 import org.openide.WizardDescriptor;
+import org.openide.WizardValidationException;
 import org.openide.util.HelpCtx;
 
 /**
@@ -10,9 +11,10 @@ import org.openide.util.HelpCtx;
  * @author Pascal Lindner
  */
 
-public class ScheduledLegWizard implements WizardDescriptor.Panel<WizardDescriptor> {
+public class ScheduledLegWizard implements WizardDescriptor.ValidatingPanel<WizardDescriptor> {
 
     private ScheduledLegPanel component;
+//    private boolean isValid = false;
 
     @Override
     public ScheduledLegPanel getComponent() {
@@ -48,6 +50,13 @@ public class ScheduledLegWizard implements WizardDescriptor.Panel<WizardDescript
     @Override
     public void storeSettings(WizardDescriptor wiz) {
         wiz.putProperty("leg", component.getSchedueldLeg());
+    }
+
+    @Override
+    public void validate() throws WizardValidationException {
+        if (component.getSchedueldLeg().getAlternative() == null){
+            throw new WizardValidationException(null, "Alternative leg is missing", null);
+        }
     }
 
 }
