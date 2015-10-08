87eb6a1e9d439d5ab4ddfcd21990ffcefbe4d614
Miguel Gonzalez <miguel.gonzalez@centerdevice.com>
2015-10-08 11:07:11 +0200

    LIMO-230: introduce LIMO-utils module

---
+++ LIMO-utils/src/main/java/nl/fontys/sofa/limo/validation/BeanValidator.java
@@ -0,0 +1,13 @@
+package nl.fontys.sofa.limo.validation;
+
+/**
+ * Validates beans according to their annotations and state.
+ * 
+ * @author Miguel Gonzalez <m.gonzalez@student.fontys.nl>
+ */
+public class BeanValidator {
+    
+    public static <T> void validate(T bean) {
+        
+    }
+}
