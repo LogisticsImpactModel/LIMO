73e58885aab27b7ca60c24a99710e2ccb9ad11c7
Miguel Gonzalez <miguel.gonzalez@centerdevice.com>
2015-10-08 12:08:10 +0200

    LIMO-230: add Junit test skeleton for validation tests

--- LIMO-utils/src/main/java/nl/fontys/sofa/limo/validation/BeanValidator.java
+++ LIMO-utils/src/main/java/nl/fontys/sofa/limo/validation/BeanValidator.java
@@ -1,13 +1,31 @@
 package nl.fontys.sofa.limo.validation;
 
+import java.lang.reflect.Field;
+
 /**
  * Validates beans according to their annotations and state.
  * 
  * @author Miguel Gonzalez <m.gonzalez@student.fontys.nl>
  */
 public class BeanValidator {
-    
-    public static <T> void validate(T bean) throws ValidationException {
+    public BeanValidator() {
         
+    }
+    
+    /**
+     * Validates beans and throws exception on bad result.
+     * 
+     * @param bean bean to validate
+     * @throws ValidationException is thrown on bad validation
+     */
+    public static <T> void validate(T bean) throws ValidationException {
+        Class<?> clazz = bean.getClass();
+        Field[] fields = clazz.getFields();
+        for (Field field : fields) {
+            System.out.println(field.getName());
+        }
+        // for each field
+        // -> check annotation type (if type is not fitting show logger warning)
+        // -> check current value and throw exception if necessary
     }
 }
---
+++ LIMO-utils/src/test/java/nl/fontys/sofa/limo/validation/BeanValidatorTest.java
@@ -0,0 +1,87 @@
+package nl.fontys.sofa.limo.validation;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author miguel
+ */
+public class BeanValidatorTest {
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testAssertFalse() {
+    
+    }
+    
+    @Test
+    public void testAssertTrue() {
+    
+    }
+    
+    @Test
+    public void testDecimalMin() {
+    
+    }
+    
+    @Test
+    public void testDecimalMax() {
+    
+    }
+    
+    @Test
+    public void testDigits() {
+    
+    }
+    
+    @Test
+    public void testPast() {
+    
+    }
+    
+    @Test
+    public void testFuture() {
+    
+    }
+    
+    @Test
+    public void testMin() {
+    
+    }
+    
+    @Test
+    public void testMax() {
+    
+    }
+    
+    @Test
+    public void testNotNull() {
+    
+    }
+    
+    @Test
+    public void testNull() {
+    
+    }
+    
+    @Test
+    public void testPattern() {
+    
+    }
+    
+    @Test
+    public void testSize() {
+    
+    }
+}
