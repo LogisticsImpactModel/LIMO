16202ae8f9c166d57cc32bbaf270439a1eaa238d
TechLogic <nils.heyer@gmx.de>
2015-11-13 15:06:08 +0100

    Change icon to new one

--- view/src/main/java/nl/fontys/sofa/limo/view/wizard/export/ExportWizardAction.java
+++ view/src/main/java/nl/fontys/sofa/limo/view/wizard/export/ExportWizardAction.java
@@ -37,7 +37,7 @@ import org.openide.awt.ActionRegistration;
 )
 @ActionRegistration(
         displayName = "Export Master Data...",
-        iconBase = "icons/gui/database.png"
+        iconBase = "icons/gui/Database.png"
 )
 @ActionReferences({
     @ActionReference(path = "Menu/Master Data", position = 60, separatorBefore = 55),
